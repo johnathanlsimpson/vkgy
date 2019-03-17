08204439604ef0b2958c3729eca4a8241c097096
John Simpson <johnathan.l.simpson@gmail.com>
2019-03-17 01:09:02 -0400

    Added sample database variables file.

---
+++ php/database-variables.php
@@ -0,0 +1,10 @@
+<?php
+// Set database connection variables here; used in /php/database-connect.php
+
+/*
+	$pdo_host     = 'localhost';
+	$pdo_dbname   = '';
+	$pdo_username = '';
+	$pdo_password = '';
+	$pdo_charset  = 'utf8';
+*/
