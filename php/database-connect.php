ded0444cf5a860e6d2113e2acef7287385a1e5db
John Simpson <johnathan.l.simpson@gmail.com>
2019-03-20 01:59:00 -0400

    Added blank database-variables. Began work on auto database setup.

--- php/database-connect.php
+++ php/database-connect.php
@@ -1,30 +1,39 @@
 <?php
-	if(!isset($pdo) || !$pdo->getAttribute(PDO::ATTR_CONNECTION_STATUS)) {
-		include_once("../php/database-variables.php");
+require '../php/database-variables.php';
+
+if(!isset($pdo) || !$pdo->getAttribute(PDO::ATTR_CONNECTION_STATUS)) {
+	$pdo_dsn      = "mysql:host=$pdo_host;dbname=$pdo_dbname1;charset=$pdo_charset";
+	$pdo_options  = array(
+		PDO::ATTR_ERRMODE            => PDO::ERRMODE_SILENT,
+		PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+		PDO::ATTR_EMULATE_PREPARES   => false,
+		PDO::ATTR_PERSISTENT         => false
+	);
+
+	try {
+		$pdo = new PDO($pdo_dsn, $pdo_username, $pdo_password, $pdo_options);
+	}
+	catch(PDOException $e) {
+		file_put_contents ("../errors/mysql".str_replace ("/", "|", $_SERVER ["REQUEST_URI"].$_SERVER["PATH_INFO"].$_SERVER["QUERY_STRING"])."-".$_SERVER["REQUEST_METHOD"]."-".$_SERVER["REMOTE_ADDR"], $e->getMessage());
 		
-		$pdo_dsn      = "mysql:host=$pdo_host;dbname=$pdo_dbname;charset=$pdo_charset";
-		$pdo_options  = array(
-			PDO::ATTR_ERRMODE            => PDO::ERRMODE_SILENT,
-			PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-			PDO::ATTR_EMULATE_PREPARES   => false,
-			PDO::ATTR_PERSISTENT         => false
-		);
-		
-		try {
-			$pdo = new PDO($pdo_dsn, $pdo_username, $pdo_password, $pdo_options);
-		}
-		catch(PDOException $e) {
-			file_put_contents ("../errors/mysql".str_replace ("/", "|", $_SERVER ["REQUEST_URI"].$_SERVER["PATH_INFO"].$_SERVER["QUERY_STRING"])."-".$_SERVER["REQUEST_METHOD"]."-".$_SERVER["REMOTE_ADDR"], $e->getMessage());
-			
-			if($e->getMessage() === "SQLSTATE[HY000] [2002] Can't connect to local MySQL server through socket '/var/lib/mysql/mysql.sock' (2 \"No such file or directory\")") {
-				$pdo_host = "127.0.0.1";
-				try {
-					$pdo = new PDO($pdo_dsn, $pdo_username, $pdo_password, $pdo_options);
-				}
-				catch(PDOException $e) {
-					file_put_contents ("../errors/mysql".str_replace ("/", "|", $_SERVER ["REQUEST_URI"].$_SERVER["PATH_INFO"].$_SERVER["QUERY_STRING"])."-".$_SERVER["REQUEST_METHOD"]."-".$_SERVER["REMOTE_ADDR"], $e->getMessage());
-				}
+		if($e->getMessage() === "SQLSTATE[HY000] [2002] Can't connect to local MySQL server through socket '/var/lib/mysql/mysql.sock' (2 \"No such file or directory\")") {
+			$pdo_host = "127.0.0.1";
+			try {
+				$pdo = new PDO($pdo_dsn, $pdo_username, $pdo_password, $pdo_options);
+			}
+			catch(PDOException $e) {
+				file_put_contents ("../errors/mysql".str_replace ("/", "|", $_SERVER ["REQUEST_URI"].$_SERVER["PATH_INFO"].$_SERVER["QUERY_STRING"])."-".$_SERVER["REQUEST_METHOD"]."-".$_SERVER["REMOTE_ADDR"], $e->getMessage());
 			}
 		}
 	}
-?>
+	
+	if($pdo) {
+		$sql_check_db = "SHOW DATABASES LIKE '".$pdo_dbname."'";
+		$stmt_check_db = $pdo->prepare($sql_check_db);
+		$stmt_check_db->execute();
+		if(!$stmt_check_db->fetch()) {
+			include('../php/database-create.php');
+			create_vkgy_database($pdo);
+		}
+	}
+}
--- php/database-variables.php
+++ php/database-variables.php
@@ -1,10 +1,8 @@
 <?php
 // Set database connection variables here; used in /php/database-connect.php
 
-/*
-	$pdo_host     = 'localhost';
-	$pdo_dbname   = '';
-	$pdo_username = '';
-	$pdo_password = '';
-	$pdo_charset  = 'utf8';
-*/
+$pdo_host     = 'localhost';
+$pdo_dbname   = 'vkgy_dummy';
+$pdo_username = '';
+$pdo_password = '';
+$pdo_charset  = 'utf8';
