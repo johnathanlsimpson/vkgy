96075c1434da250d1271c026d3997c15403f451b
John Simpson <johnathan.l.simpson@gmail.com>
2019-04-15 15:01:09 -0400

    [Bug fix] Corrected SQL to pull username of latest updates on database page; previously was pulling username of op.

--- database/index.php
+++ database/index.php
@@ -7,36 +7,56 @@ $sql_recent = "
 	SELECT recent.*, users.username
 	FROM (
 		(
-			SELECT 'artist' AS type, edits_artists.user_id, CONCAT_WS('/', '', 'artists', friendly, '') AS url, COALESCE(romaji, name) AS quick_name, MAX(edits_artists.date_occurred) AS date_edited, '' AS artist_quick_name, '' AS artist_url
+			SELECT
+				'artist' AS type, edits_artists.user_id, CONCAT_WS('/', '', 'artists', friendly, '') AS url, COALESCE(romaji, name) AS quick_name, edits_artists.date_occurred AS date_edited, '' AS artist_quick_name, '' AS artist_url
 			FROM edits_artists
+			INNER JOIN (
+				SELECT MAX(id) AS id FROM edits_artists GROUP BY artist_id ORDER BY id DESC LIMIT 20
+			) max_edit_artist_ids
+			ON edits_artists.id=max_edit_artist_ids.id
 			LEFT JOIN artists ON artists.id=edits_artists.artist_id
-			GROUP BY edits_artists.artist_id
-			ORDER BY MAX(edits_artists.date_occurred) DESC LIMIT 20
 		)
 		UNION
 		(
-			SELECT 'company' AS type, edits_labels.user_id, CONCAT_WS('/', '', 'labels', friendly, '') AS url, COALESCE(romaji, name) AS quick_name, MAX(edits_labels.date_occurred) AS date_edited, '' AS artist_quick_name, '' AS artist_url
+			SELECT
+				'company' AS type, edits_labels.user_id, CONCAT_WS('/', '', 'labels', friendly, '') AS url, COALESCE(romaji, name) AS quick_name, edits_labels.date_occurred AS date_edited, '' AS artist_quick_name, '' AS artist_url
 			FROM edits_labels
+			INNER JOIN (
+				SELECT MAX(id) AS id FROM edits_labels GROUP BY label_id ORDER BY id DESC LIMIT 20
+			) max_edit_label_ids
+			ON edits_labels.id=max_edit_label_ids.id
 			LEFT JOIN labels ON labels.id=edits_labels.label_id
-			GROUP BY edits_labels.label_id
-			ORDER BY MAX(edits_labels.date_occurred) DESC LIMIT 20
 		)
 		UNION
 		(
-			SELECT 'musician' AS type, edits_musicians.user_id, CONCAT_WS('/', '', 'musicians', musicians.id, musicians.friendly, '') AS url, COALESCE(romaji, name) AS quick_name, MAX(edits_musicians.date_occurred) AS date_edited, '' AS artist_quick_name, '' AS artist_url
+			SELECT
+				'musician' AS type,
+				edits_musicians.user_id,
+				CONCAT_WS('/', '', 'musicians', musicians.id, musicians.friendly, '') AS url, COALESCE(romaji, name) AS quick_name, edits_musicians.date_occurred AS date_edited, '' AS artist_quick_name, '' AS artist_url
 			FROM edits_musicians
+			INNER JOIN (
+				SELECT MAX(id) AS id FROM edits_musicians GROUP BY musician_id ORDER BY id DESC LIMIT 20
+			) max_edit_musician_ids
+			ON edits_musicians.id=max_edit_musician_ids.id
 			LEFT JOIN musicians ON musicians.id=edits_musicians.musician_id
-			GROUP BY edits_musicians.musician_id
-			ORDER BY MAX(edits_musicians.date_occurred) DESC LIMIT 20
 		)
 		UNION
 		(
-			SELECT 'release' AS type, edits_releases.user_id, CONCAT_WS('/', '', 'releases', artists.friendly, releases.id, releases.friendly, '') AS url, CONCAT_WS(' ', COALESCE(releases.romaji, releases.name, ''), COALESCE(press_romaji, press_name, ''), COALESCE(type_romaji, type_name, '')) AS quick_name, MAX(edits_releases.date_occurred) AS date_edited, COALESCE(artists.romaji, artists.name) AS artist_quick_name, CONCAT_WS('/', '', 'artists', artists.friendly, '') AS artist_url
+			SELECT
+				'release' AS type,
+				edits_releases.user_id,
+				CONCAT_WS('/', '', 'releases', artists.friendly, releases.id, releases.friendly, '') AS url,
+				CONCAT_WS(' ', COALESCE(releases.romaji, releases.name, ''), COALESCE(press_romaji, press_name, ''), COALESCE(type_romaji, type_name, '')) AS quick_name,
+				edits_releases.date_occurred AS date_edited,
+				COALESCE(artists.romaji, artists.name) AS artist_quick_name,
+				CONCAT_WS('/', '', 'artists', artists.friendly, '') AS artist_url
 			FROM edits_releases
+			INNER JOIN (
+				SELECT MAX(id) AS id FROM edits_releases GROUP BY release_id ORDER BY id DESC LIMIT 20
+			) max_edit_release_ids
+			ON edits_releases.id=max_edit_release_ids.id
 			LEFT JOIN releases ON releases.id=edits_releases.release_id
 			LEFT JOIN artists ON artists.id=releases.artist_id
-			GROUP BY edits_releases.release_id
-			ORDER BY MAX(edits_releases.date_occurred) DESC LIMIT 20
 		)
 	) AS recent
 	LEFT JOIN users ON users.id=recent.user_id
