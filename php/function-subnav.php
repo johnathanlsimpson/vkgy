3479469c081031cf742cb77b6b8fa7b4474344a9
John Simpson <johnathan.l.simpson@gmail.com>
2019-06-07 00:32:58 -0400

    [Feature] Updated subnav function to allow both types of subnavs.

--- php/function-subnav.php
+++ php/function-subnav.php
@@ -1,20 +1,27 @@
-<?php
-	function subnav($input_array, $signed_in_only = false) {
-		global $subnavs;
-		if(!empty($input_array)) {
-			foreach($input_array as $key => $value) {
-				if(!empty($key) && !empty($value)) {
-					$tmp_subnavs[] = [
-						"text" => $key,
-						"url" => $value,
-						"signed_in_only" => $signed_in_only ? true : false
-					];
-				}
-			}
-			
-			if(is_array($tmp_subnavs) && !empty($tmp_subnavs)) {
-				$subnavs[] = $tmp_subnavs;
-			}
-		}
-	}
-?>
+<?php
+
+// Take array of text/url pairs and add to navigation arrays
+function subnav($input_array, $nav_type = 'section', $signed_in_only = false) {
+	global $section_nav, $interact_nav;
+
+	if(!empty($input_array)) {
+		foreach($input_array as $key => $value) {
+			if(!empty($key) && !empty($value)) {
+				$tmp_subnavs[] = [
+					'text' => $key,
+					'url' => $value,
+					'signed_in_only' => $signed_in_only ? true : false
+				];
+			}
+		}
+		
+		if(is_array($tmp_subnavs) && !empty($tmp_subnavs)) {
+			if($nav_type === 'section') {
+				$section_nav[] = $tmp_subnavs;
+			}
+			elseif($nav_type === 'interact') {
+				$interact_nav[] = $tmp_subnavs;
+			}
+		}
+	}
+}
