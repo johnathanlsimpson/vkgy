c7011737caa20dc8ffc1f42add39bd301a9519c4
John Simpson <johnathan.l.simpson@gmail.com>
2019-06-07 00:27:26 -0400

    [Change] Updated 404 page for new header.

--- 404/index.php
+++ 404/index.php
@@ -1,28 +1,15 @@
-<?php
-	$request = urldecode(substr($_SERVER["REQUEST_URI"], 1));
-	$request = str_replace("/", " ", $request);
-	$request = preg_replace("/"."\s$"."/", "", $request);
-	
-	breadcrumbs([
-		"Page not found" => "/404/",
-		"Search" => "/search/"
-	]);
-	
-	$pageTitle = "Page not found";
-?>
-
-<div class="col c1">
-	<div>
-		<div class="text text--outlined text--notice symbol__error">
-			Sorry, the requested page couldn't be found. <?php echo $request ? "Instead, here's a search for &ldquo;".sanitize($request)."&rdquo;" : "Try a search instead."; ?>
-		</div>
-	</div>
-</div>
-
-<?php
-	if($request && $request !== "404 ") {
-		$search["q"] = $request;
-	}
-	
-	include("../search/page-index.php");
-?>
+<?php
+$request = urldecode(substr($_SERVER["REQUEST_URI"], 1));
+$request = str_replace(['/', '-'], ' ', $request);
+$request = trim($request);
+$request = $_SERVER['REQUEST_URI'] === '/404/' && $request === '404' ? null : $request;
+
+$page_title = 'Page not found (エラー)';
+
+$error = 'Sorry, the requested page couldn\'t be found. '.($request ? 'Here\'s a search for &ldquo;'.sanitize($request).'&rdquo;.' : 'Try a search instead.');
+
+if(strlen($request)) {
+	$search['q'] = $request;
+}
+
+include('../search/index.php');
