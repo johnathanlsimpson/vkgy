d73b6c5ec2086f2ae464bbbf3ec32ca8f2a25064
John Simpson <johnathan.l.simpson@gmail.com>
2019-06-06 20:54:56 -0400

    [Bug fix] Reinstated language-switch script that was accidentally deleted.

--- php/page-index.php
+++ php/page-index.php
@@ -137,6 +137,28 @@
 			</div>
 		</nav>
 		
+		<script>
+			let enButton = document.querySelector('[for="language-en"]');
+			let jaButton = document.querySelector('[for="language-ja"]');
+			
+			function setLang(event) {
+				let url = '/php/function-set_lang.php';
+				let lang = 'en';
+				let connection = new XMLHttpRequest();
+				
+				if(event.target.htmlFor === 'language-ja') {
+					lang = 'ja';
+				}
+				
+				connection.open('POST', url);
+				connection.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+				connection.send("lang=" + lang);
+			}
+			
+			enButton.addEventListener('click', setLang);
+			jaButton.addEventListener('click', setLang);
+		</script>
+		
 		<form action="/search/" class="any--hidden" enctype="multipart/form-data" id="form__search" method="get" name="form__search"><button type="submit"></button></form>
 		
 		<div class="header__wrapper  col c1">
