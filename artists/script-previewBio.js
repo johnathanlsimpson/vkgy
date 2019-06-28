5d96f22d4da4c5d41bed5bbb4bc128958f438adc
John Simpson <johnathan.l.simpson@gmail.com>
2019-06-28 01:51:41 -0400

    [Change] Added rate limiter.

--- artists/script-previewBio.js
+++ artists/script-previewBio.js
@@ -1,18 +1,27 @@
-function previewBio(inputElem, targetElem, artistId) {
-	var content  = $(inputElem).val();
-	var formData = new FormData();
-	
-	formData.append("content", content);
-	formData.append("artist", artistId);
-	
-	$.ajax({
-		url:         "/artists/function-preview_bio.php",
-		data:        formData,
-		processData: false,
-		contentType: false,
-		type:        "post"
-	})
-	.done(function(returnedData) {
-		$(targetElem).html(returnedData);
-	});
+var previewTimeout = null;
+
+function previewBio(inputElem, targetElem, artistId) {
+	if(previewTimeout != null) {
+		clearTimeout(previewTimeout);
+	}
+	
+	previewTimeout = setTimeout(function() {
+		var content  = $(inputElem).val();
+		var formData = new FormData();
+		
+		formData.append("content", content);
+		formData.append("artist", artistId);
+		
+		$.ajax({
+			url:         "/artists/function-preview_bio.php",
+			data:        formData,
+			processData: false,
+			contentType: false,
+			type:        "post"
+		})
+		.done(function(returnedData) {
+			$(targetElem).html(returnedData);
+		});
+		
+	}, 250);
 }
