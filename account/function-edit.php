e9042d6028d7685f1d332b0bb00f3d839536fa24
John Simpson <johnathan.l.simpson@gmail.com>
2019-10-13 20:09:16 -0400

    [Feature] Added VIP-limited ability to change username symbol. (Will appear across the site in limited instances for now.)

--- account/function-edit.php
+++ account/function-edit.php
@@ -5,7 +5,7 @@ include_once('../php/include.php');
 if($_SESSION['is_signed_in']) {
 	
 	// Clean & set user preferences
-	foreach(['name', 'motto', 'email', 'website', 'twitter', 'facebook', 'lastfm', 'tumblr', 'fan_since', 'site_theme', 'gender'] as $key) {
+	foreach(['name', 'motto', 'email', 'website', 'twitter', 'facebook', 'lastfm', 'tumblr', 'fan_since', 'site_theme', 'gender', 'icon'] as $key) {
 		$sql_values[$key] = sanitize($_POST[$key]);
 		$sql_values[$key] = strlen($sql_values[$key]) ? $sql_values[$key] : null;
 	}
@@ -13,6 +13,8 @@ if($_SESSION['is_signed_in']) {
 	// Further clean some values
 	$email_pattern = '/^(?!(?:(?:\x22?\x5C[\x00-\x7E]\x22?)|(?:\x22?[^\x5C\x22]\x22?)){255,})(?!(?:(?:\x22?\x5C[\x00-\x7E]\x22?)|(?:\x22?[^\x5C\x22]\x22?)){65,}@)(?:(?:[\x21\x23-\x27\x2A\x2B\x2D\x2F-\x39\x3D\x3F\x5E-\x7E]+)|(?:\x22(?:[\x01-\x08\x0B\x0C\x0E-\x1F\x21\x23-\x5B\x5D-\x7F]|(?:\x5C[\x00-\x7F]))*\x22))(?:\.(?:(?:[\x21\x23-\x27\x2A\x2B\x2D\x2F-\x39\x3D\x3F\x5E-\x7E]+)|(?:\x22(?:[\x01-\x08\x0B\x0C\x0E-\x1F\x21\x23-\x5B\x5D-\x7F]|(?:\x5C[\x00-\x7F]))*\x22)))*@(?:(?:(?!.*[^.]{64,})(?:(?:(?:xn--)?[a-z0-9]+(?:-[a-z0-9]+)*\.){1,126}){1,}(?:(?:[a-z][a-z0-9]*)|(?:(?:xn--)[a-z0-9]+))(?:-[a-z0-9]+)*)|(?:\[(?:(?:IPv6:(?:(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){7})|(?:(?!(?:.*[a-f0-9][:\]]){7,})(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){0,5})?::(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){0,5})?)))|(?:(?:IPv6:(?:(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){5}:)|(?:(?!(?:.*[a-f0-9]:){5,})(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){0,3})?::(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){0,3}:)?)))?(?:(?:25[0-5])|(?:2[0-4][0-9])|(?:1[0-9]{2})|(?:[1-9]?[0-9]))(?:\.(?:(?:25[0-5])|(?:2[0-4][0-9])|(?:1[0-9]{2})|(?:[1-9]?[0-9]))){3}))\]))$/iD';
 	$sql_values['site_theme'] = is_numeric($sql_values['site_theme']) ? $sql_values['site_theme'] : 0;
+	$sql_values['icon'] = is_numeric($sql_values['icon']) ? $sql_values['icon'] : 0;
+	$sql_values['icon'] = $sql_values['icon'] > 0 && !$_SESSION['is_vip'] ? 0 : $sql_values['icon'];
 	$sql_values['email'] = preg_match($email_pattern, $sql_values['email']) ? $sql_values['email'] : null;
 	
 	// If email doesn't match pattern, make note
