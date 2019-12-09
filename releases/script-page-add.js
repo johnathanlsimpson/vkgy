131eb215aa277e0595f99f5c25e60200f56b8b53
John Simpson <johnathan.l.simpson@gmail.com>
2019-12-08 22:00:44 -0500

    [Feature] Updated script on 'add release' page to better filter out leading periods/colons, and to handle certain inline tracklists. Also updated it to automatically grab romanization for certain songs if they're already in the song list.

--- releases/script-page-add.js
+++ releases/script-page-add.js
@@ -245,59 +245,153 @@ function cleanSongTitle(inputTitle) {
 	return inputTitle;
 }
 
+
 // Paste tracklist from clipboard
-document.addEventListener("paste", function(event) {
+document.addEventListener('paste', function(event) {
 	if(event.target.getAttribute("name") === "tracklist[name][]") {
+		
+		// Grab paste text, clean slightly, then check if it matches tracklist pattersn
 		var pasteText = event.clipboardData.getData('text/plain');
-		pasteText = pasteText.split("\n");
-		pasteText = pasteText.filter(function(x) { return x.replace(/\s+/, ''); });
+		var tracklistPattern, numPastedLines;
+		pasteText = pasteText.trim();
 		
-		var numPastedLines = pasteText.length;
+		// NUMBERS_AT_BOUNDARY (starts with number, plus multiple instances of numbers after a word boundary)
+		if( pasteText.match(/^\d+/) && (pasteText.match(/\b\d+/g) || []).length > 1 ) {
+			tracklistPattern = 'NUMBERS_AT_BOUNDARY';
+		}
 		
-		if(numPastedLines > 1) {
-			event.preventDefault();
+		// MULTI_LINE
+		else if(pasteText.includes('\n')) {
+			tracklistPattern = 'MULTI_LINE';
+		}
+		
+		// If tracklist pattern detected, separate into lines, then clean
+		if(tracklistPattern) {
 			
-			var currElem = event.target;
-			
-			// Check if all lines are wrapped by 「」
-			var checkForBrackets = false;
-			var firstTrack = cleanSongTitle(pasteText[0]);
-			var secondTrack = cleanSongTitle(pasteText[1]);
-			var bracketText = '「」';
-			if(firstTrack.slice(0,1) + firstTrack.slice(-1) === bracketText && secondTrack.slice(0,1) + secondTrack.slice(-1) === bracketText) {
-				checkForBrackets = true;
+			// Split tracks
+			if(tracklistPattern === 'MULTI_LINE') {
+				pasteText = pasteText.split('\n');
+			}
+			else if(tracklistPattern === 'NUMBERS_AT_BOUNDARY') {
+				pasteText = pasteText.split(/\b\d+/);
 			}
 			
-			for(var i=0; i<numPastedLines; i++) {
-				var cleanedTitle = cleanSongTitle(pasteText[i]);
+			// Remove empty values, set line count
+			pasteText = pasteText.filter(function(x) { return x.replace(/\s+/, ''); });
+			numPastedLines = pasteText.length;
+			
+			// If multiple lines, prevent paste event and handle song insertion
+			if(numPastedLines > 1) {
 				
-				if(checkForBrackets) {
-					if(cleanedTitle.slice(0,1) + cleanedTitle.slice(-1) === bracketText) {
-						cleanedTitle = cleanedTitle.slice(1,-1);
-					}
+				// Prevent event, set target
+				var currElem = event.target;
+				event.preventDefault();
+				
+				// Get current song options so we can auto-romanize
+				var songOptions = document.querySelector('[data-contains="songs"]');
+				songOptions = JSON.parse(songOptions.innerHTML);
+				
+				// Check if first and second track begin with ., :, or variant, and both begin with same one; if so, assume all do & set flag to remove later
+				var firstTrack = cleanSongTitle(pasteText[0]);
+				var secondTrack = cleanSongTitle(pasteText[1]);
+				var checkForPeriod;
+				if( firstTrack.slice(0,1) === secondTrack.slice(0,1) && firstTrack.slice(0,1).match(/[.．。:：]/) && secondTrack.slice(0,1).match(/[.．。:：]/) ) {
+					checkForPeriod = true;
 				}
 				
-				currElem.value = currElem.value + cleanedTitle;
+				// If first and second track are bracketed (after accounting for leading period), assume all are & set flag to remove later
+				var bracketText = '「」';
+				var checkForBrackets;
+				if(checkForPeriod) {
+					firstTrack = firstTrack.replace(/^[.．。:：]\s*/, '');
+					secondTrack = secondTrack.replace(/^[.．。:：]\s*/, '');
+				}
+				if(firstTrack.slice(0,1) + firstTrack.slice(-1) === bracketText && secondTrack.slice(0,1) + secondTrack.slice(-1) === bracketText) {
+					checkForBrackets = true;
+				}
 				
-				if(i + 1 < numPastedLines) {
-					var isParent = false;
+				// Loop through each track and transform + insert it
+				for(var i=0; i<numPastedLines; i++) {
 					
-					while(!isParent) {
-						currElem = currElem.parentElement;
-						
-						if(currElem.classList.contains("track--show-song")) {
-							var addTrackButton = currElem.querySelector(".track__song-controls:last-of-type .track__song-control");
-							addTrackButton.click();
-							currElem = currElem.nextElementSibling;
-							currElem = currElem.querySelector("input[name^=\"tracklist\[name\]\"]");
-							isParent = true;
+					// Clean song title (remove spaces, etc)
+					var cleanedTitle = cleanSongTitle(pasteText[i]);
+					
+					// Remove periods if necessary
+					if(checkForPeriod) {
+						if(cleanedTitle.slice(0,1).match(/[.．。:：]/)) {
+							cleanedTitle = cleanedTitle.replace(/^[.．。:：]\s*/, '');
 						}
 					}
+					
+					// Remove brackets if necessary
+					if(checkForBrackets) {
+						if(cleanedTitle.slice(0,1) + cleanedTitle.slice(-1) === bracketText) {
+							cleanedTitle = cleanedTitle.slice(1,-1);
+						}
+					}
+					
+					// Set element to pasted song title
+					currElem.value = currElem.value + cleanedTitle;
+					
+					// Check if song title is in list of possible songs, insert romanization into neighbor if so
+					songOptions.forEach(function(songOption, index) {
+						if(songOption.name === cleanedTitle) {
+							insertNameAndRomaji( currElem, splitNameAndRomaji(songOption.quick_name) );
+						}
+					});
+					
+					// If not at end of tracklist, make sure we don't run out of empty inputs
+					if(i + 1 < numPastedLines) {
+						var isParent = false;
+						
+						// Move up to parent .track--show-song container, so we can move to next one
+						while(!isParent) {
+							currElem = currElem.parentElement;
+							
+							// If on .track--show-song container, check if sibling is also .track--show-song and has empty name input
+							if(currElem.classList.contains("track--show-song")) {
+								
+								// If next .track--show-song exists and has empty name & romaji inputs, let next track fill that
+								var nextElem = currElem.nextElementSibling;
+								if(nextElem && nextElem.classList.contains('track--show-song') && nextElem.querySelector('[name="tracklist[name][]"]').value.length === 0) {
+								}
+								
+								// Otherwise, press the 'add track' button and focus the newly-created name element, so next track can go there
+								else {
+									var addTrackButton = currElem.querySelector('.track__song-controls:last-of-type .track__song-control');
+									addTrackButton.click();
+								}
+								
+								// Select next name field and end loop
+								currElem = currElem.nextElementSibling;
+								currElem = currElem.querySelector('[name="tracklist[name][]"]');
+								
+								isParent = true;
+							}
+						}
+					}
+					
 				}
+				
+				// After pasting all items, let's make sure the last one is focused (may be romaji or may be name)
+				if(currElem) {
+					var parentElem = currElem.parentElement.classList.contains('.track__song') ? currElem.parentElement : currElem.parentElement.parentElement;
+					var romajiElem = parentElem.querySelector('[name="tracklist[romaji][]"]');
+					
+					if(romajiElem.value) {
+						romajiElem.focus();
+					}
+					else {
+						currElem.focus();
+					}
+				}
+				
 			}
 		}
+		
 	}
 });
+
 
 // Clear tracklist
 let clearButton = document.querySelector('[data-clear]');
