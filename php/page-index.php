4d124d6b6554e30533bd659496b70416f4689f68
John Simpson <johnathan.l.simpson@gmail.com>
2020-01-08 22:50:14 -0500

    [Change] Changed default social card bg on pages with no set image. Changed order so that page background isn't taken from card image.

--- php/page-index.php
+++ php/page-index.php
@@ -3,8 +3,8 @@
 	$page_description = $page_description ? $page_description." | vkgy (ブイケージ)" : "vkgy is a visual kei library maintained by overseas fans. vkgy（ブイケージ）はビジュアル系のファンサイトとライブラリです。関連するアーティストのメンバープロフィールや活動やリリース情報などがあります。";
 	$page_title = $page_title ?: $pageTitle;
 	$page_title = $page_title ? $page_title.' | vkgy (ブイケージ)' : 'vkgy (ブイケージ) | visual kei library (V系ライブラリ)';
-	$page_image = $page_image ?: 'https://vk.gy/support/patreon-back.png';
 	$background_image = $background_image ?: ($page_image ?: null);
+	$page_image = $page_image ?: 'https://vk.gy/style/card.png';
 	
 	style([
 		"../style/style-tooltips.css"
