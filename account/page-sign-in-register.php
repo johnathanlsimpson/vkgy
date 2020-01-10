25c96e8cba692424ae0fef0d47fcf2d09461ca5a
John Simpson <johnathan.l.simpson@gmail.com>
2020-01-09 23:58:38 -0500

    [Bug fix] Corrected text of avatar selection when signing up.

--- account/page-sign-in-register.php
+++ account/page-sign-in-register.php
@@ -77,7 +77,7 @@
 							<div class="input__group" style="align-self: center;">
 								<label class="input__label"><?php echo lang('Avatar eyes', 'アバター', ['secondary_class' => 'any--hidden']); ?></label>
 								<label class="input__checkbox-label symbol__unchecked register__bat" for="register__radio--bat"><?php echo lang('bat', 'メークⅠ', ['secondary_class' => 'any--hidden']); ?></label>
-								<label class="input__checkbox-label symbol__unchecked register__gecko" for="register__radio--gecko"><?php echo lang('bat', 'メークⅡ', ['secondary_class' => 'any--hidden']); ?></label>
+								<label class="input__checkbox-label symbol__unchecked register__gecko" for="register__radio--gecko"><?php echo lang('gecko', 'メークⅡ', ['secondary_class' => 'any--hidden']); ?></label>
 							</div>
 							<div class="input__group">
 								<div class="register__face"></div>
