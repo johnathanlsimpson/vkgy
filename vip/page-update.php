4b23ba92ff37b17ca95fc1dabe7891fd027ce20a
John Simpson <johnathan.l.simpson@gmail.com>
2020-01-18 15:47:28 -0500

    [Change] Updated 'edit VIP post' form to include option to set friendly name. Also added delete function, and updated back/forward links to use newer function.

--- vip/page-update.php
+++ vip/page-update.php
@@ -4,6 +4,7 @@
 			"/scripts/external/script-autosize.js",
 			'/scripts/external/script-tribute.js',
 			'/scripts/script-initTribute.js',
+			'/scripts/script-initDelete.js',
 			"/vip/script-page-update.js"
 		]);
 		
@@ -18,38 +19,69 @@
 			'Update' => '/vip/add/',
 		]);
 		
+		// Previous entry
+		$sql_prev = "SELECT friendly, title FROM vip WHERE id < ? ORDER BY id DESC LIMIT 1";
+		$stmt_prev = $pdo->prepare($sql_prev);
+		$stmt_prev->execute([$entry["id"]]);
+		$prev = $stmt_prev->fetch();
+		if(is_array($prev) && !empty($prev)) {
+			subnav([
+				[
+					'text' => $prev['title'],
+					'url' => '/vip/'.$prev['friendly'].'/edit/',
+					'position' => 'left',
+				],
+			], 'directional');
+		}
+		
+		// Next entry
+		$sql_next = "SELECT friendly, title FROM vip WHERE id > ? ORDER BY id ASC LIMIT 1";
+		$stmt_next = $pdo->prepare($sql_next);
+		$stmt_next->execute([$entry["id"]]);
+		$next = $stmt_next->fetch();
+		if(is_array($next) && !empty($next)) {
+			subnav([
+				[
+					'text' => $next['title'],
+					'url' => '/vip/'.$next['friendly'].'/edit/',
+					'position' => 'right',
+				],
+			], 'directional');
+		}
+		
 		?>
 			<form action="/vip/function-update.php" class="col c1" enctype="multipart/form-data" method="post" name="form__update">
-			
 				
 				<div class="col c3-AAB">
 					<div>
 						<h2>
-							Update <a class="a--inherit" href="/vip/<?php echo $entry["friendly"]; ?>/"><?php echo $entry["title"]; ?></a>
+							Update <a class="a--inherit" href="<?= '/vip/'.$entry["friendly"]; ?>/"><?= $entry["title"]; ?></a>
 						</h2>
 						<div class="text">
-							<input name="id" type="hidden" value="<?php echo $entry["id"]; ?>" />
-
+							<input name="id" type="hidden" value="<?= $entry["id"]; ?>" />
+							
 							<div class="input__row">
 								<div class="input__group any--flex-grow">
 									<label class="input__label">Title</label>
-									<input class="any--flex-grow" name="title" placeholder="title" value="<?php echo $entry["title"]; ?>" />
+									<input class="any--flex-grow" name="title" placeholder="title" value="<?= $entry["title"]; ?>" />
+									<input class="input--secondary" name="friendly" placeholder="friendly" value="<?= $entry['friendly']; ?>" />
 								</div>
 							</div>
-
+							
 							<div class="input__row">
 								<div class="input__group any--flex-grow">
 									<label class="input__label">Entry</label>
-									<textarea class="autosize input__textarea any--flex-grow any--tributable" data-is-previewed="true" name="content" placeholder="your entry here"><?php echo $entry["content"]; ?></textarea>
+									<textarea class="autosize input__textarea any--flex-grow any--tributable" data-is-previewed="true" name="content" placeholder="your entry here"><?= $entry["content"]; ?></textarea>
 								</div>
 							</div>
-
+							
 							<div class="input__row">
 								<div class="input__group any--flex-grow" data-role="submit-container">
 									<button class="any--flex-grow" name="submit" type="submit">
 										Submit
 									</button>
 									<span data-role="status"></span>
+									<button class="symbol__trash" data-role="delete" name="delete" type="button" style="margin-left:1ch;"></button>
 								</div>
 								
 								<div class="input__group any--flex-grow any--flex any--hidden" data-role="edit-container">
@@ -57,7 +89,7 @@
 									<a class="a--padded" data-get="edit_url" data-get-into="href" data-role="edit">Edit</a>
 								</div>
 							</div>
-
+							
 							<div class="text text--outlined text--notice update__result" data-role="result"></div>
 						</div>
 					</div>
@@ -73,43 +105,6 @@
 								<div class="update__preview"></div>
 							</div>
 						</div>
-					</div>
-				</div>
-				
-				<div class="col c2">
-					<div>
-						<?php
-							$sql_prev = "SELECT friendly, title FROM vip WHERE id < ? ORDER BY id DESC LIMIT 1";
-							$stmt_prev = $pdo->prepare($sql_prev);
-							$stmt_prev->execute([$entry["id"]]);
-							$prev = $stmt_prev->fetch();
-							
-							if(is_array($prev) && !empty($prev)) {
-								?>
-									<a href="/vip/<?php echo $prev["friendly"]; ?>/edit/">
-										<span class="symbol__previous"></span>
-										<?php echo $prev["title"]; ?>
-									</a>
-								<?php
-							}
-						?>
-					</div>
-					<div class="any--align-right">
-						<?php
-							$sql_next = "SELECT friendly, title FROM vip WHERE id > ? ORDER BY id ASC LIMIT 1";
-							$stmt_next = $pdo->prepare($sql_next);
-							$stmt_next->execute([$entry["id"]]);
-							$next = $stmt_next->fetch();
-							
-							if(is_array($next) && !empty($next)) {
-								?>
-									<a class="any--align-right" href="/vip/<?php echo $next["friendly"]; ?>/edit/">
-										<?php echo $next["title"]; ?>
-										<span class="symbol__next"></span>
-									</a>
-								<?php
-							}
-						?>
 					</div>
 				</div>
 			</form>
--- vip/script-page-update.js
+++ vip/script-page-update.js
@@ -1,7 +1,8 @@
+// Init autosize and submit
+// --------------------------------------------------------
 autosize($(".autosize"));
 
-
-initializeInlineSubmit($("[name=form__update]"), "/vip/function-update_entry.php", { "submitOnEvent" : "submit", "showEditLink" : true });
+initializeInlineSubmit($("[name=form__update]"), "/vip/function-update.php", { "submitOnEvent" : "submit", "showEditLink" : true });
 
 
 // Preview entry
@@ -27,4 +28,36 @@ var typingTimer;
 $("[name=content]").on("input propertychange change paste", function() {
 	clearTimeout(typingTimer);
 	typingTimer = setTimeout(previewEntry, 200);
+});
+
+
+// Delete entry
+// --------------------------------------------------------
+var idElem = document.querySelector('[name="id"]');
+var deleteId = idElem.value;
+var deleteElem = document.querySelector('[data-role="delete"]');
+
+initDelete($(deleteElem), '/vip/function-delete.php', { id : deleteId }, function() {
+	$('body').removeClass('any--pulse').addClass('any--pulse');
+	
+	var formElems = document.querySelectorAll('[name="form__update"] input');
+	formElems.forEach((elem) => {
+		elem.value = '';
+	});
+	
+	var formLink = document.querySelector('[name="form__update"] h2 a');
+	formLink.setAttribute('href', '');
+	formLink.innerHTML = '';
+	
+	var formTextareas = document.querySelectorAll('[name="form__update"] .input__textarea');
+	formTextareas.forEach((elem) => {
+		elem.value = '';
+		elem.innerHTML = '';
+	});
+	
+	var formPreview = document.querySelector('.update__preview');
+	formPreview.innerHTML = '';
+	
+	deleteElem.innerHTML = '';
+	deleteElem.classList.remove('symbol__success');
 });
