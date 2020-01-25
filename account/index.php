0ab8aa210a4a841d6a545141f3bb548b792e1b05
John Simpson <johnathan.l.simpson@gmail.com>
2020-01-25 00:36:15 -0500

    [Change] Updated account index to use template switcher.

--- account/index.php
+++ account/index.php
@@ -6,42 +6,67 @@ $access_release = new access_release($pdo);
 
 include('../account/head.php');
 
-	// Switch controller
-	if(!$template && $_GET["page"] === "users") {
-		$template = "users";
+// Switch controller
+switch(true) {
+	case($_GET['page'] === 'users'):
+		$template = 'users'; break;
+		
+	case($_GET['page'] === 'account' && $_SESSION['is_signed_in']):
+		$template = 'account'; break;
+		
+	case($_GET['page'] === 'user' && strlen($_GET['username'])):
+		$template = 'user'; break;
+		
+	case($_GET['page'] === 'activity' && strlen($_GET['username'])):
+		$template = 'activity'; break;
+		
+	case($_GET['action'] === 'download' && strlen($_GET['username']) && $_GET['username'] === $_SESSION['username']):
+		$template = 'download-collection'; break;
+		
+	case($_GET['page'] === 'edit-avatar' && $_SESSION['is_signed_in']):
+		$template = 'edit-avatar'; break;
+		
+	case($_GET['page'] === 'account' && !$_SESSION['is_signed_in']):
+		$template = 'sign-in'; break;
+		
+	case($_GET['action'] === 'sign-out'):
+		$template = 'sign-out'; break;
+		
+	default:
+		$template = 'users';
+}
+
+// Check if requested user exists, change template if necessary
+if(in_array($template, [ 'account', 'activity', 'user' ])) {
+	$user_check = $access_user->access_user([ 'username' => sanitize($_GET['username']), 'get' => 'name' ]);
+	
+	if(is_array($user_check) && !empty($user_check)) {
+		$user = $access_user->access_user([ 'username' => sanitize($_GET['username']), 'get' => 'all' ]);
 	}
-	if(!$template && $_GET["action"] === "sign-out") {
-		$template = "sign-out";
+	else {
+		$error = 'Couldn\'t find the requested user. Showing all users instead.';
+		$template = 'users';
 	}
-	if(!$template && !empty($_GET["username"]) && $_GET["action"] === "download" && $_SESSION["username"] === $_GET["username"]) {
-		$template = "download-collection";
-	}
-	if(!$template && !empty($_GET["username"])) {
-		$user = $access_user->access_user(["username" => sanitize($_GET["username"]), "get" => "all"]);
-		if(is_array($user) && !empty($user)) {
-			$template = "user";
-		}
-		else {
-			$error = "Sorry, that user doesn't exist. Showing all users instead.";
-			$template = "users";
-		}
-	}
-	if(!$template && $_SESSION["loggedIn"]) {
-		if($_GET['page'] === 'edit-avatar') {
-			$template = 'edit-avatar';
-		}
-		else {
-			$user = $access_user->access_user(["username" => sanitize($_SESSION["username"]), "get" => "all"]);
-			if(is_array($user) && !empty($user)) {
-				$template = "account";
-			}
-			else {
-				$error = "Sorry, that user doesn't exist. Showing all users instead.";
-				$template = "users";
-			}
-		}
-	}
-	$template = strlen($template) ? $template : "sign-in";
+}
+
+// Get avatar if necessary
+if(in_array($template, [ 'activity', 'edit-avatar', 'user' ])) {
+	include_once('../avatar/class-avatar.php');
+	include_once('../avatar/avatar-definitions.php');
+	
+	$sql_avatar = "SELECT content FROM users_avatars WHERE user_id=? LIMIT 1";
+	$stmt_avatar = $pdo->prepare($sql_avatar);
+	$stmt_avatar->execute([ (is_numeric($user['id']) ? $user['id'] : $_SESSION['user_id']) ]);
+	$rslt_avatar = $stmt_avatar->fetchColumn();
+	
+	$avatar_class = $rslt_avatar ? null : 'user--no-avatar';
+	$rslt_avatar = $rslt_avatar ?: '{"head__base":"default","head__base-color":"i"}';
+	
+	$avatar = new avatar($avatar_layers, $rslt_avatar, ["is_vip" => true]);
+	$user['avatar'] = $avatar->get_avatar_paths();
+	
+	unset($avatar);
+}
 	
 	// User list
 	// =======================================================
@@ -71,7 +96,7 @@ include('../account/head.php');
 		include("page-download-collection.php");
 	}
 	
-	// User profile
+	// User profile: top
 	// =======================================================
 	if($template === "user") {
 		$pageTitle = $user["username"]." member profile";
@@ -79,6 +104,16 @@ include('../account/head.php');
 		breadcrumbs([ "Member list" => "/users/", $user["username"] => "/users/".$user["username"]."/" ]);
 		
 		include('page-user.php');
+	}
+	
+	// User profile: activity
+	// =======================================================
+	if($template === 'activity') {
+		$page_title = $user['user'].' activity';
+		
+		breadcrumbs([ 'Member list' => '/users/', $user['username'] => '/users/'.$user['username'].'/', 'Activity' => '/users/'.$user['username'].'/activity/' ]);
+		
+		include('page-activity.php');
 	}
 	
 	// Edit profile
