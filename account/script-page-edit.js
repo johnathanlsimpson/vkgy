bf4894ea169631578496b4cf78cedc7a8a226c82
John Simpson <johnathan.l.simpson@gmail.com>
2020-04-09 12:12:45 -0400

    [Bug fix] Updated account edit script to take into account that some fields will be hidden depending on view.

--- account/script-page-edit.js
+++ account/script-page-edit.js
@@ -20,31 +20,38 @@ var themeZeroButton = document.getElementById('site_theme_0');
 var themeOneButton = document.getElementById('site_theme_1');
 var themeCSSLink = document.getElementById('stylesheet_theme');
 
-themeZeroButton.onclick = function(event) {
-	themeCSSLink.setAttribute('href', '/style/style-colors-0.css');
-};
-themeOneButton.onclick = function(event) {
-	themeCSSLink.setAttribute('href', '/style/style-colors-1.css');
-};
+// This button may not exist if only editing user roles
+if(themeZeroButton) {
+	themeZeroButton.onclick = function(event) {
+		themeCSSLink.setAttribute('href', '/style/style-colors-0.css');
+	};
+	themeOneButton.onclick = function(event) {
+		themeCSSLink.setAttribute('href', '/style/style-colors-1.css');
+	};
+}
 
 
 // Show/hide pronouns option
 // --------------------------------------------------------
 var pronounsSelector = document.querySelector('[name="pronouns"]');
 var pronounsElem = document.querySelector('[name="custom_pronouns"]');
-if(pronounsSelector.value === 'custom') {
-	pronounsElem.classList.remove('any--hidden');
-}
-pronounsSelector.addEventListener('change', function(event) {
-	console.log('a'); console.log(pronounsSelector.value);
-	if(pronounsSelector.value === 'custom') { console.log('b');
+
+// This button may not exist if only editing user roles
+if(pronounsSelector) {
+	if(pronounsSelector.value === 'custom') {
 		pronounsElem.classList.remove('any--hidden');
-		pronounsElem.focus();
 	}
-	else { console.log('c');
-		pronounsElem.classList.add('any--hidden');
-	}
-});
+	pronounsSelector.addEventListener('change', function(event) {
+		console.log('a'); console.log(pronounsSelector.value);
+		if(pronounsSelector.value === 'custom') { console.log('b');
+			pronounsElem.classList.remove('any--hidden');
+			pronounsElem.focus();
+		}
+		else { console.log('c');
+			pronounsElem.classList.add('any--hidden');
+		}
+	});
+}
 
 
 // Move/update tooltip with "fan since" range
@@ -52,8 +59,44 @@ pronounsSelector.addEventListener('change', function(event) {
 let sinceElem = document.querySelector('.fan-since__input');
 let sinceLabel = document.querySelector('.fan-since__tooltip');
 
-sinceElem.addEventListener('change', () => {
-	let sinceValue = sinceElem.value;
-	sinceLabel.innerHTML = sinceValue > sinceElem.min ? sinceValue : '~' + sinceValue;
-	sinceLabel.style.setProperty('--fan-since', sinceValue);
-});
+// This button may not exist if only editing user roles
+if(sinceElem) {
+	sinceElem.addEventListener('change', () => {
+		let sinceValue = sinceElem.value;
+		sinceLabel.innerHTML = sinceValue > sinceElem.min ? sinceValue : '~' + sinceValue;
+		sinceLabel.style.setProperty('--fan-since', sinceValue);
+	});
+}
+
+
+// Associate permissions with roles
+// --------------------------------------------------------
+// Check if there's a permission field on page (meaning we're allowed to set permissions)
+if(document.querySelector('[name^="can_"]')) {
+	
+	let isPairings = {
+		'is_vip' : [ 'can_access_drafts' ],
+		'is_editor' : [ 'can_add_data' ],
+		'is_moderator' : [ 'can_approve_data', 'can_delete_data', 'can_edit_roles' ]
+	};
+	let isElems = document.querySelectorAll('[name^="is_"]');
+	let isName, isChecked;
+	
+	// Every time role input is changed, get name and status
+	isElems.forEach(function(elem) {
+		elem.addEventListener('change', function() {
+			
+			isName = elem.name;
+			isChecked = elem.checked;
+			
+			// Check/unchecked associated permission inputs
+			if(isPairings[isName]) {
+				isPairings[isName].forEach(function(canName) {
+					document.querySelector('[name="' + canName + '"]').checked = isChecked;
+				});
+			}
+			
+		});
+	});
+	
+}
