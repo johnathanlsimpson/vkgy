f73a4787b1dc4256b4fbf5653990b9ba10d647ab
John Simpson <johnathan.l.simpson@gmail.com>
2020-04-18 21:57:24 -0400

    [Bug fix] Corrected typo in image upload box.

--- images/template-upload.php
+++ images/template-upload.php
@@ -18,7 +18,7 @@
 						</label>
 						
 						<div style="margin-top:1rem;text-align:center;">
-							<span class="any--weaken-color">Or paste or image here:</span>
+							<span class="any--weaken-color">Or paste image here:</span>
 							<input placeholder="paste image or URL" name="image_url" />
 						</div>
 						
