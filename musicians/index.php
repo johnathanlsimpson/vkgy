3e04c125c802bd13093394b0be6b2d77fa726051
John Simpson <johnathan.l.simpson@gmail.com>
2020-04-19 22:38:14 -0400

    [Change] Changed musician page to use new tags.

--- musicians/index.php
+++ musicians/index.php
@@ -19,30 +19,35 @@
 		$musician_id = sanitize($_GET["musician"]);
 		$musician = $access_musician->access_musician(["id" => $musician_id, "get" => "all"]);
 		
-		// Tags
-		$sql_tags = "SELECT * FROM tags_musicians ORDER BY friendly ASC";
-		$stmt_tags = $pdo->prepare($sql_tags);
-		$stmt_tags->execute();
-		$rslt_tags = $stmt_tags->fetchAll();
+		// Get tags
+		$item_type = 'musician';
+		$item_id = $musician['id'];
+		include_once('../tags/function-get_tags.php');
+		$tags = get_tags($pdo, $item_type, $item_id);
 		
-		$sql_curr_tags = "SELECT tags_musicians.*, COUNT(musicians_tags.id) AS num_times_tagged FROM musicians_tags LEFT JOIN tags_musicians ON tags_musicians.id=musicians_tags.tag_id WHERE musicians_tags.musician_id=? GROUP BY musicians_tags.tag_id";
-		$stmt_curr_tags = $pdo->prepare($sql_curr_tags);
-		$stmt_curr_tags->execute([ $musician["id"] ]);
-		$rslt_curr_tags = $stmt_curr_tags->fetchAll();
-		
-		if(is_array($rslt_curr_tags) && !empty($rslt_curr_tags)) {
-			foreach($rslt_curr_tags as $tag) {
-				$needs_admin_tags = $needs_admin_tags ?: ($tag["is_admin_tag"] ?: false);
-				$rslt_curr_tag_ids[] = $tag["id"];
-				
-				if($tag['friendly'] === 'exclusive') {
-					$musician_is_exclusive = true;
-				}
-				
-				if($tag['friendly'] === 'removed') {
-					$musician_is_removed = true;
+		// Loop through tags and do some stuff
+		if(is_array($tags) && !empty($tags)) {
+			
+			$all_tags = $tags['all_tags'];
+			$current_tags = $tags['current_tags'];
+			$user_tags = $tags['user_tags'];
+			$tag_types = $tags['tag_types'];
+			
+			// Loop through current tags and set some flags for artist
+			if(is_array($current_tags) && !empty($current_tags)) {
+				foreach($current_tags as $numeric_key => $tag) {
+					
+					// Set flags
+					if($tag['friendly'] === 'exclusive') {
+						$musician_is_exclusive = true;
+					}
+					if($tag['friendly'] === 'removed') {
+						$musician_is_removed = true;
+					}
+					
 				}
 			}
+			
 		}
 		
 		if(is_array($musician) && !empty($musician)) {
