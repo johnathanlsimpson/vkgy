8318b4100d3b88c018907e6285e21ab5cd40cb4b
John Simpson <johnathan.l.simpson@gmail.com>
2020-07-30 12:05:09 -0400

    [Bug fix] Corrected delete button on 'edit article' page.

--- blog/page-update.php
+++ blog/page-update.php
@@ -713,7 +713,7 @@
 					
 					<!-- Delete -->
 					<?php $delete_button_class = $_SESSION['can_delete_data'] || $_SESSION['user_id'] === $entry['user_id'] ? null : 'any--hidden'; ?>
-					<label class="input__radio symbol__trash symbol--standalone <?= $delete_button_class; ?>" data-get="id" data-get-into="data-id" data-id="<?= $entry["id"]; ?>" data-translation="<?= $entry['is_translation'] ? 1 : 0 ;?>" name="delete"></label>
+					<label class="input__radio symbol__trash symbol--standalone <?= $delete_button_class; ?>" data-get="id" data-get-into="data-id" data-id="<?= $entry["id"]; ?>" data-is-translation="<?= $entry['is_translation'] ? 1 : 0 ;?>" name="delete"></label>
 					
 					<a class="save__link symbol__arrow-right-circled" href="<?= $entry_url; ?>" target="_blank">
 						<m1>Preview draft</m1>
