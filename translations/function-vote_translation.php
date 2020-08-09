a1df48ddf4a5b98787c07398194498445bb900a9
John Simpson <johnathan.l.simpson@gmail.com>
2020-08-09 00:38:19 -0400

    [Change] Updated vote_translation to auto accept/reject and build translation file.

--- translations/function-vote_translation.php
+++ translations/function-vote_translation.php
@@ -1,6 +1,7 @@
 <?php
 
 include_once('../php/include.php');
+include_once('../translations/function-generate_translations.php');
 
 $proposal_id = is_numeric($_POST['id']) ? $_POST['id'] : null;
 $vote_type = in_array($_POST['vote_type'], ['upvote', 'downvote']) ? $_POST['vote_type'] : null;
@@ -152,41 +153,6 @@ if($_SESSION['is_signed_in']) {
 				
 			}
 			
-			// Update translations for that particular page
-			function generate_translation_file($folder, $language, $pdo) {
-				
-				if(strlen($folder) && file_exists('../'.$folder)) {
-					
-					$sql_translations = '
-						SELECT
-							translations.*,
-							translations_proposals.content AS translation
-						FROM
-							translations
-						LEFT JOIN
-							translations_proposals ON translations_proposals.id=translations.'.$language.'_id
-						WHERE
-							translations.folder=? AND translations.'.$language.'_id IS NOT NULL
-					';
-					$stmt_translations = $pdo->prepare($sql_translations);
-					$stmt_translations->execute([ $folder ]);
-					$rslt_translations = $stmt_translations->fetchAll();
-					
-					$translations = [];
-					
-					if(is_array($rslt_translations) && !empty($rslt_translations)) {
-						foreach($rslt_translations as $translation) {
-							$translations[ $translation['content'] ] = $translation['translation'];
-						}
-					}
-					
-					$translation_file = gzcompress( serialize( $translations ) );
-					$filename = '../'.$folder.'/lang.'.$language;
-					file_put_contents( $filename, $translation_file );
-					
-				}
-				
-			}
 			generate_translation_file('translations', $rslt_proposal['language'], $pdo);
 			
 			$output['language'] = $rslt_proposal['language'];
