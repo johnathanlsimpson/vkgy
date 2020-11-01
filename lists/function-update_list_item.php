1a1605878f34cf967fc94bf03797b14eb787651d
John Simpson <johnathan.l.simpson@gmail.com>
2020-11-01 17:59:46 -0500

    [Bug fix] Fixed errors with normal lists vs own/want/sell.

--- lists/function-update_list_item.php
+++ lists/function-update_list_item.php
@@ -1,6 +1,9 @@
 <?php
 
 include_once('../php/include.php');
+include_once('../php/class-access_list.php');
+
+$access_list = new access_list($pdo);
 
 // Only signed-in users can add items to lists
 if($_SESSION['is_signed_in']) {
@@ -11,36 +14,32 @@ if($_SESSION['is_signed_in']) {
 	$list_id   = sanitize($_POST['list_id']);
 	$user_id   = $_SESSION['user_id'];
 	$action    = $_POST['item_is_listed'] ? 'add' : 'delete';
-	$is_sell   = $list_id == 2;
-	
-	// Only allow certain kinds of items in lists
-	$allowed_item_types = [
-		'release',
-		'video'
-	];
-	
+	$is_sell   = $list_id == -3;
+
 	// Set up points
 	$access_points = new access_points($pdo);
-	
+
 	// Eventually, we'll award a generic list point (probably); for now let's set it up for releases
-	$point_type = $list_id == 0 ? 'wanted-release' : ( $list_type == 1 ? 'collected-release' : ($list_type == 2 ? 'sold-release' : null) );
+	$point_type = $list_id == -2 ? 'wanted-release' : ( $list_id == -1 ? 'collected-release' : ($list_id == -3 ? 'sold-release' : null) );
+	
+	// Only allow certain kinds of items in lists
+	$allowed_item_types = array_keys(access_list::$allowed_item_types);
 	
 	// Make sure list and item are specified, and item is allowed
 	if( is_numeric($item_id) && is_numeric($list_id) && in_array($item_type, $allowed_item_types) ) {
 		
 		// Eventually, all lists will be in one table (maybe?); for now, let's set up alternate tables
 		$list_tables = [
-			'releases_collections',
-			'releases_wants',
-			'releases_collections',
+			-1 => 'releases_collections',
+			-2 => 'releases_wants',
+			-3 => 'releases_collections',
 		];
 		
 		// !! Start release lists
-		if( $list_id < 3 ) {
+		if( $list_id < 0 ) {
 		
 		// Check current status of item in list
 		$sql_check = 'SELECT id FROM '.$list_tables[$list_id].' WHERE user_id=? AND '.$item_type.'_id=? LIMIT 1';
-		
 		$stmt_check = $pdo->prepare($sql_check);
 		$stmt_check->execute([ $user_id, $item_id ]);
 		$extant_id = $stmt_check->fetchColumn();
@@ -81,7 +80,6 @@ if($_SESSION['is_signed_in']) {
 			
 			$sql_update = 'UPDATE '.$list_tables[$list_id].' SET is_for_sale=? WHERE id=?';
 			$stmt_update = $pdo->prepare($sql_update);
-			
 			if($stmt_update->execute([ ($action === 'add' ? 1 : 0), $extant_id ])) {
 				$output['status'] = 'success';
 				$output['result'] = 'Updated selling status.';
