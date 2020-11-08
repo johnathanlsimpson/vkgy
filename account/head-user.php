4ef1ef9a679612616f249afae43174cba92a82f2
John Simpson <johnathan.l.simpson@gmail.com>
2020-11-08 13:43:45 -0500

    [Change] Changed list navigation in user accounts.

--- account/head-user.php
+++ account/head-user.php
@@ -5,11 +5,9 @@ subnav([
 	tr('Activity', ['ja' => '活動']) => '/users/'.$user['username'].'/activity/',
 ]);
 
-if($_SESSION['is_vip']) {
-	subnav([
-		tr('Lists', ['ja' => 'リスト']).' (&beta;)' => '/users/'.$user['username'].'/lists/',
-	]);
-}
+subnav([
+	tr('Lists', ['ja' => 'リスト']).' (&beta;)' => '/users/'.$user['username'].'/lists/',
+]);
 
 if($_SESSION['username'] === $user['username']) {
 	subnav([
