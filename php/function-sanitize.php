5c931205b3e29d29a360d7fc166aa4e5c77d6d73
John Simpson <johnathan.l.simpson@gmail.com>
2020-11-27 11:24:12 -0500

    [Change] Rewrote portions of sanitize() to make it easier to understand, and added some hyphen standardization.

--- php/function-sanitize.php
+++ php/function-sanitize.php
@@ -1,42 +1,69 @@
 <?php
-	function standardize($input) {
-		$input = mb_convert_kana($input, "aKV", "UTF-8");
-		$input = str_replace(["˜", "∼", "～", "〜"], "~", $input);
-		$input = str_replace("…", "・・・", $input);
-		$input = str_replace(['　', "\t"], ' ', $input);
-		$input = trim($input, " \t\0\x0B");
+
+// Standardize input
+function standardize($input) {
+	
+	// Make kana full width
+	$input = mb_convert_kana($input, "aKV", "UTF-8");
+	
+	// Define standard symbols and the variants to standardize
+	$symbols = [
 		
-		/*if(substr_count($input, '"') === 2) {
-			$input = preg_replace('/'.'"'.'/', '“', $input, 1);
-			$input = preg_replace('/'.'"'.'/', '”', $input, 1);
-		}
+		// Tildes
+		'~' => [
+			'˜', '∼', '～', '〜', '⁓'
+		],
 		
-		if(substr_count($input, '\'') === 2) {
-			$input = preg_replace('/'.'\''.'/', '‘', $input, 1);
-			$input = preg_replace('/'.'\''.'/', '’', $input, 1);
-		}*/
+		// Hyphens
+		'-' => [
+			'‐', '‑', '‒', '–', '―', '−', '⸺', '⸻', '﹘', '﹣', '－',
+		],
 		
-		return $input;
+		// Ambiguous periods
+		'・・・' => [
+			'…',
+		],
+		
+		// Horizontal spaces
+		' ' => [
+			"\h",
+		],
+		
+	];
+	
+	// Standardize symbols
+	foreach($symbols as $replacement => $searches) {
+		$input = str_replace( $searchs, $replacement, $input );
 	}
 	
-	function sanitize($input, $modifier = NULL) {
-		$input = standardize($input);
-		
-		$search = array("?", "\\", "<", ">", "\"", "'", '﻿');
-		$replace = array("&#63;", "&#92;", "&#60;", "&#62;", "&#34;", "&#39;", '');
-		
-		if($modifier != "allowhtml") {
-			$input = str_replace($search, $replace, $input);
-		}
-		
-		$input = mb_convert_encoding($input, "HTML-ENTITIES", "UTF-8");
-		
-		if($modifier != "allowslash") {
-			if(@mysqli_ping()) {
-				$input = mysqli_real_escape_string($GLOBALS["mysqli"], $input);
-			}
-		}
-		
-		return($input);
+	// Trim whitespace
+	$input = trim($input, " \t\0\x0B");
+	
+	return $input;
+	
+}
+
+// Transform text to entities
+function sanitize($input, $modifier = NULL) {
+	
+	// Standardize
+	$input = standardize($input);
+	
+	$search = [ "?", "\\", "<", ">", "\"", "'", '﻿' ];
+	$replace = [ "&#63;", "&#92;", "&#60;", "&#62;", "&#34;", "&#39;", '' ];
+	
+	if($modifier != "allowhtml") {
+		$input = str_replace($search, $replace, $input);
 	}
-?>
+	
+	$input = mb_convert_encoding($input, "HTML-ENTITIES", "UTF-8");
+	
+	if($modifier != "allowslash") {
+		if(@mysqli_ping()) {
+			$input = mysqli_real_escape_string($GLOBALS["mysqli"], $input);
+		}
+	}
+	
+	return($input);
+	
+}
