2e21d27c56bd04df475970ccf00a4b66e76ac03c
John Simpson <johnathan.l.simpson@gmail.com>
2020-12-07 19:33:56 -0500

    [Feature] Completely redid tagging backend. Tags are now separated from votes, which allows us to disable voting for some tags, hide or pin tags, more easily query tagged artists, etc.

--- tags/partial-tags.php
+++ tags/partial-tags.php
@@ -1,11 +1,15 @@
 <?php
-	script([
-		'/tags/script-partial-tags.js',
-	]);
-	
-	style([
-		'/tags/style-partial-tags.css',
-	]);
+
+include_once('../votes/function-render_vote.php');
+
+script([
+	'/tags/script-partial-tags.js',
+]);
+
+style([
+	'/tags/style-partial-tags.css',
+]);
+
 ?>
 
 <h3>
@@ -13,82 +17,114 @@
 </h3>
 <ul class="text text--outlined">
 	<?php
-		if(is_array($current_tags) && !empty($current_tags)) {
-			foreach($tag_types as $tag_type) {
-				
-				if(is_array($current_tags[$tag_type]) && !empty($current_tags[$tag_type])) {
-					?>
-						<li class="tag__wrapper">
-							<?php
-								echo '<div class="h5">'.$tag_type.'</div>';
-								
-								$num_of_type = count($current_tags[$tag_type]);
-								foreach($current_tags[$tag_type] as $tag_key => $tag) {
-									if($tag_type === 'subgenres') {
-										?>
-											<div class="tag__container tag--subgenre">
-												<a class="text <?= 'tag--'.$tag['friendly']; ?>" href="<?= '/search/'.$item_type.'s/?tags[]='.$tag['friendly']; ?>">
-													<span class="tag__thumbnail lazy"></span>
-													<span class="tag__name">
-														<?= $tag['romaji'] ? lang($tag['romaji'], $tag['name'], 'div') : $tag['name']; ?>
-													</span>
-												</a>
-												<?php
-													echo '
-														<span class="tag__voting any--weaken-color">
-															
-															<label class="tag__vote tag__upvote" data-vote="upvote" data-id="'.$item_id.'" data-tag-id="'.$tag['id'].'" data-item-type="'.$item_type.'">
-																<input class="input__choice" type="checkbox" '.(is_array($user_upvotes) && in_array($tag['id'], $user_upvotes) ? 'checked' : null).' />
-																<span class="tag__status symbol__up-caret symbol--standalone"></span>
-															</label>
-															
-															<span class="tag__num any--weaken-size" data-tag-id="'.$tag['id'].'" data-num-tags="'.$tag['num_upvotes'].'"></span>
-															
-															<label class="tag__vote tag__status tag__downvote" data-vote="downvote" data-id="'.$item_id.'" data-tag-id="'.$tag['id'].'" data-item-type="'.$item_type.'">
-																<input class="input__choice" type="checkbox" '.(is_array($user_downvotes) && in_array($tag['id'], $user_downvotes) ? 'checked' : null).' />
-																<span class="symbol__down-caret symbol--standalone"></span>
-															</label>
-															
-														</span>
-													';
-												?>
-											</div>
-										<?php
-									}
-									else {
-										?>
-											<span class="tag__container">
-												<a class="" href="<?= '/search/'.$item_type.'s/?tags[]='.$tag['friendly']; ?>">
-													<?= lang($tag['romaji'] ?: $tag['name'], $tag['name'], 'hidden'); ?>
-												</a>
-												<?php
-													echo '
-														<span class="tag__voting any--weaken-color">
-															
-															<label class="tag__vote tag__upvote" data-vote="upvote" data-id="'.$item_id.'" data-tag-id="'.$tag['id'].'" data-item-type="'.$item_type.'">
-																<input class="input__choice" type="checkbox" '.(is_array($user_upvotes) && in_array($tag['id'], $user_upvotes) ? 'checked' : null).' />
-																<span class="tag__status symbol__up-caret symbol--standalone"></span>
-															</label>
-															
-															<span class="tag__num any--weaken-size" data-tag-id="'.$tag['id'].'" data-num-tags="'.$tag['num_upvotes'].'"></span>
-															
-															<label class="tag__vote tag__status tag__downvote" data-vote="downvote" data-id="'.$item_id.'" data-tag-id="'.$tag['id'].'" data-item-type="'.$item_type.'">
-																<input class="input__choice" type="checkbox" '.(is_array($user_downvotes) && in_array($tag['id'], $user_downvotes) ? 'checked' : null).' />
-																<span class="symbol__down-caret symbol--standalone"></span>
-															</label>
-															
-														</span>
-													';
-												?>
-											</span>
-										<?php
-									}
+		if(is_array($tags['tagged']) && !empty($tags['tagged'])) {
+			foreach($tags['tagged'] as $tag_type => $tagged_tags) {
+				?>
+					<li class="tags__wrapper">
+						
+						<div class="h5"><?= $tag_type; ?></div>
+						
+						<?php
+							foreach($tagged_tags as $tag_key => $tag) {
+								if($tag_type === 'scenes') {
+									?>
+										<div class="tag__container tag--scene">
+											
+											<a class="text <?= 'tag--'.$tag['friendly']; ?>" href="<?= '/search/'.$item_type.'s/?tags[]='.$tag['friendly']; ?>">
+												<span class="tag__thumbnail lazy"></span>
+												<span class="tag__name">
+													<?= $tag['romaji'] ? lang($tag['romaji'], $tag['name'], 'div') : $tag['name']; ?>
+												</span>
+											</a>
+											
+											<?php
+												// Only mods can pin/hide
+												if( $_SESSION['can_approve_data'] ) {
+													
+													echo '<div class="tag__moderation--vertical">';
+													
+													// All moderation tags can be pinned
+													?><label class="tag__moderation tag--pin input__checkbox" data-direction="pin" data-items-tags-id="<?= $tag['items_tags_id']; ?>" data-item-type="artists_tags">
+														<input class="tag__pin input__choice" type="checkbox" <?= $tag['mod_score'] > 0 ? 'checked' : null; ?> />
+														<span class="symbol__pin tag__status"></span>
+													</label><?php
+													
+													// Don't show hide button if tag can only be added by moderators
+													if( $tag['requires_permission'] != 'can_approve_data' ) {
+														?><label class="tag__moderation tag--hide input__checkbox" data-direction="hide" data-items-tags-id="<?= $tag['items_tags_id']; ?>" data-item-type="artists_tags">
+															<input class="tag__hide input__choice" type="checkbox" <?= $tag['mod_score'] < 0 ? 'checked' : null; ?> />
+															<span class="symbol__hide tag__status"></span>
+														</label><?php
+													}
+													
+													echo '</div>';
+													
+												}
+												
+												if( is_numeric($tag['score']) && $tag['mod_score'] != 1 ) {
+													echo render_component($vote_template, [
+														'direction_class' => 'vote--vertical',
+														'item_id' => $tag['items_tags_id'],
+														'item_type' => 'artists_tags',
+														'upvote_is_checked' => $tag['user_score'] > 0 ? 'checked' : null,
+														'downvote_is_checked' => $tag['user_score'] < 0 ? 'checked' : null,
+														'score' => $tag['score'],
+													]);
+												}
+											?>
+											
+										</div>
+									<?php
 								}
-							?>
-						</li>
-					<?php
-				}
-				
+								else {
+									?>
+										<span class="tag__container">
+											
+											<a href="<?= '/search/'.$item_type.'s/?tags[]='.$tag['friendly']; ?>">
+												<?= lang($tag['romaji'] ?: $tag['name'], $tag['name'], 'hidden'); ?>
+											</a>
+											
+											<?php
+												// Only mods can pin/hide
+												if( $_SESSION['can_approve_data'] ) {
+													
+													// All moderation tags can be pinned
+													?><label class="tag__moderation tag--pin input__checkbox" data-direction="pin" data-items-tags-id="<?= $tag['items_tags_id']; ?>" data-item-type="artists_tags">
+														<input class="tag__pin input__choice" type="checkbox" <?= $tag['mod_score'] > 0 ? 'checked' : null; ?> />
+														<span class="symbol__pin tag__status"></span>
+													</label><?php
+													
+													// Don't show hide button if tag can only be added by moderators
+													if( $tag['requires_permission'] != 'can_approve_data' ) {
+														?><label class="tag__moderation tag--hide input__checkbox" data-direction="hide" data-items-tags-id="<?= $tag['items_tags_id']; ?>" data-item-type="artists_tags">
+															<input class="tag__hide input__choice" type="checkbox" <?= $tag['mod_score'] < 0 ? 'checked' : null; ?> />
+															<span class="symbol__hide tag__status"></span>
+														</label><?php
+													}
+													
+												}
+												
+												// Only signed in users may vote, and only on tags that allow it
+												if( $_SESSION['is_signed_in'] && $tag['is_votable'] ) {
+													
+													echo render_component($vote_template, [
+														'item_id' => $tag['items_tags_id'],
+														'item_type' => 'artists_tags',
+														'upvote_is_checked' => $tag['user_score'] > 0 ? 'checked' : null,
+														'downvote_is_checked' => $tag['user_score'] < 0 ? 'checked' : null,
+														'score' => $tag['score'],
+													]);
+													
+												}
+											?>
+											
+										</span>
+									<?php
+								}
+							}
+						?>
+					</li>
+				<?php
 			}
 		}
 		else {
@@ -97,122 +133,48 @@
 	?>
 </ul>
 
-<style>
-	.tag--subgenre {
-		align-items: stretch;
-		margin-bottom: 1rem;
-	}
-	.tag--subgenre .text {
-		display: flex;
-		flex: 1;
-		margin-bottom: 0;
-	}
-	
-	.tag__voting {
-		border: 1px solid hsl(var(--background));
-		border-radius: 3px;
-		display: inline-flex;
-		line-height: 1.5rem;
-		text-align: center;
-	}
-	.tag--subgenre .tag__voting {
-		flex-direction: column;
-	}
-	.tag__num {
-		padding: 0 5px;
-	}
-	.tag__num::before {
-		content: attr(data-num-tags);
-	}
-	.tag__num[data-num-tags^="-"] {
-		color: red;
-	}
-	.tag__upvote, .tag__downvote {
-		color: hsl(var(--background--bold));
-		cursor: pointer;
-		padding: 0 5px;
-		position: initial;
-	}
-	.tag__upvote:hover, .tag__downvote:hover {
-		color: hsl(var(--text));
-	}
-	.tag__upvote:hover::after, .tag__downvote:hover::after {
-		background: linear-gradient(var(--vote-dir), var(--vote-bg), transparent);
-		bottom: 0;
-		content: "";
-		display: block;
-		left: 0;
-		pointer-events: none;
-		position: absolute;
-		right: 0;
-		top: 0;
-	}
-	.tag__upvote:hover {
-		--vote-bg: rgba(0,255,0,0.1);
-		--vote-dir: to right;
-	}
-	.tag__downvote:hover {
-		--vote-bg: rgba(255,0,0,0.1);
-		--vote-dir: to left;
-	}
-	.tag--subgenre .tag__upvote:hover {
-		--vote-dir: to bottom;
-	}
-	.tag--subgenre .tag__downvote:hover {
-		--vote-dir: to top;
-	}
-</style>
-
 <h3>
 	<?= lang('Add tags', 'タグする', 'div'); ?>
 </h3>
-<input class="obscure__input" id="obscure-tags" type="checkbox" <?= $_SESSION['is_signed_in'] && isset($artist) ? 'checked' : null; ?> >
+
+<?php /*<input class="obscure__input" id="obscure-tags" type="checkbox" <?= $_SESSION['is_signed_in'] && isset($artist) ? 'checked' : null; ?> >*/ ?>
 <ul class="text text--outlined obscure__container obscure--height" <?= $_SESSION['is_signed_in'] && isset($artist) ? 'style="min-height:16rem;"' : null; ?> >
 	<?php
-		if($_SESSION['is_signed_in']) {
-			if(is_array($all_tags) && !empty($all_tags)) {
-				foreach($tag_types as $tag_type) {
+		if( $_SESSION['is_signed_in'] && is_array($tags['untagged']) && !empty($tags['untagged']) ) {
+				foreach($tags['untagged'] as $tag_type => $untagged_tags) {
 					
-					// Only show section if that tag type is actually used
-					if(is_array($all_tags[$tag_type]) && !empty($all_tags[$tag_type])) {
-					
-						// Make sure user is allowed to see this type of tag
-						if($tag_type != 'admin' || $_SESSION['can_add_data']) {
-							?>
-								<li class="tag__wrapper">
-									<h5>
-										<?= $tag_type; ?>
-									</h5>
-									<?php
-										foreach($all_tags[$tag_type] as $tag_key => $tag) {
+					// Make sure user is allowed to see this type of tag
+					if($tag_type != 'admin' || $_SESSION['can_add_data']) {
+						?>
+							<li class="tags__wrapper any--weaken">
+								
+								<div class="h5"><?= $tag_type; ?></div>
+								
+								<?php
+									foreach($untagged_tags as $tag_key => $tag) {
+										
+										// Make sure user is allowed to see tag
+										if(
+											!$tag['requires_permission']
+											||
+											($tag['requires_permission'] && $_SESSION[ $tag['requires_permission'] ])
+										) {
 											
-											// Make sure user is allowed to see tag
-											if(
-												!$tag['requires_permission']
-												||
-												($tag['requires_permission'] && $_SESSION[ $tag['requires_permission'] ])
-											) {
-												
-												?>
-													<label class="tag__vote any__tag input__radio" data-vote="upvote" data-id="<?= $item_id; ?>" data-tag-id="<?= $tag['id']; ?>" data-item-type="<?= $item_type; ?>">
-														<input class="input__choice" type="checkbox" <?= in_array($tag['id'], $user_upvotes) ? 'checked' : null; ?> />
-														<span class="symbol__tag tag__status">
-															<?= str_replace( [' kei', '&#31995;' ], '', lang($tag['romaji'] ?: $tag['name'], $tag['name'], 'hidden') ); ?>
-														</span>
-													</label>
-												<?php
-												
-											}
+											?>
+												<label class="tag__label input__checkbox" data-vote="upvote" data-id="<?= $item_id; ?>" data-tag-id="<?= $tag['id']; ?>" data-item-type="<?= $item_type; ?>">
+													<input class="tag__checkbox input__choice" type="checkbox" <?= $tag['user_score'] == 1 ? 'checked' : null; ?> />
+													<span class="symbol__plus tag__status"><?= str_replace( [' kei', '&#31995;' ], '', lang($tag['romaji'] ?: $tag['name'], $tag['name'], 'hidden') ); ?></span>
+												</label>
+											<?php
+											
 										}
-									?>
-								</li>
-							<?php
-						}
-					
+									}
+								?>
+							</li>
+						<?php
 					}
 					
 				}
-			}
 		}
 		else {
 			echo lang('<a href="/account/">Sign in</a> to add tags.', 'タグするには<a href="">サインイン</a>してください。', 'hidden');
