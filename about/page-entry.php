73e5729317c726cd5b723f08528a710c683d5456
John Simpson <johnathan.l.simpson@gmail.com>
2020-12-25 19:25:53 -0500

    [Change] Changed some formatting in development log and added affected folder.

--- about/page-entry.php
+++ about/page-entry.php
@@ -20,7 +20,27 @@ $active_page = '/about/development/';
 		</div>
 		
 		<div class="text text--centered">
-			<?= $markdown_parser->parse_markdown($entry['is_issue'] ? $entry['title'] : $entry['content']); ?>
+			<?php
+				
+				// Parse Markdown
+				$content = $markdown_parser->parse_markdown($entry['is_issue'] ? $entry['title'] : $entry['content']);
+				
+				// If regular dev entry, do some additional formatting
+				if( !$entry['is_issue'] ) {
+					
+					// Format change type
+					$content = preg_replace_callback('/'.'\[(Addition|Bug fix|Change|Feature)\]'.'/', function($matches) {
+						return '<span class="any__note">'.strtolower($matches[1]).'</span>';
+					}, $content);
+					
+					// Format affected folder
+					$content = preg_replace('/'.'\<code\>([A-z]+)\<\/code\>'.'/', '<span class="any--weaken">$1</span>', $content);
+					
+				}
+				
+				echo $content;
+				
+			?>
 		</div>
 		
 	</div>
