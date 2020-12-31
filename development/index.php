9a2df9222ba66b0118fd0fdc4323f428477809d0
John Simpson <johnathan.l.simpson@gmail.com>
2020-12-31 02:01:21 -0500

    [Feature] Added multiple pages to development section.

--- development/index.php
+++ development/index.php
@@ -6,7 +6,8 @@ $access_comment = new access_comment($pdo);
 $page_header = lang('Development', 'ロードマップ', 'div');
 
 subnav([
-	'Development' => '/development/',
+	'Latest update' => '/development/',
+	'All updates' => '/development/all/',
 	'Issues' => '/development/issues/',
 ]);
 
@@ -19,22 +20,40 @@ $id = is_numeric($_GET['id']) ? $_GET['id'] : null;
 if( $template === 'issues' || $template === 'index' ) {
 	
 	$sql_issues = '
-		SELECT development.id, development.title, development.is_completed, IF(votes_development.score, SUM(votes_development.score), 0) AS score
-		FROM development
-		LEFT JOIN votes_development ON votes_development.item_id=development.id
-		WHERE development.is_issue=? AND (development.is_completed IS NULL OR development.is_completed=?)
-		GROUP BY development.id
-		ORDER BY score DESC, development.date_occurred DESC
+		SELECT issues.id, issues.title, issues.is_completed, issues.issue_type, issues.date_occurred, issues.user_id, IF(votes_development.score, SUM(votes_development.score), 0) AS score
+		FROM development issues
+		LEFT JOIN votes_development ON votes_development.item_id=issues.id
+		WHERE issues.is_issue=?
+		GROUP BY issues.id
+		ORDER BY score DESC, issues.date_occurred DESC
 	';
 	$stmt_issues = $pdo->prepare($sql_issues);
-	$stmt_issues->execute([ 1, 0 ]);
+	$stmt_issues->execute([ 1 ]);
 	$issues = $stmt_issues->fetchAll();
 	$num_issues = count($issues);
 	
-	// If no issues, and trying to view issues page, redirect to index
-	if( !$num_issues ) {
+	// If no issues, redirect to index
+	if( $template === 'issues' && !$num_issues ) {
 		$template = 'index';
 		$error = 'No issues found. Showing development index instead.';
+	}
+	
+	// Split issues by completion status
+	else {
+		
+		foreach($issues as $issue_key => $issue) {
+			
+			if($issue['is_completed']) {
+				$tmp_issues['completed'][] = $issue;
+			}
+			else {
+				$tmp_issues['incomplete'][] = $issue;
+			}
+			
+		}
+		
+		$issues = $tmp_issues;
+		
 	}
 	
 }
@@ -42,11 +61,12 @@ if( $template === 'issues' || $template === 'index' ) {
 //
 // Get entry
 //
-if( is_numeric($id) ) {
+if( is_numeric($id) || $template === 'index' ) {
 
-	$sql_entry = 'SELECT development.*, SUBSTRING(development.date_occurred, 1, 10) AS date_occurred FROM development WHERE development.id=? LIMIT 1';
+	$values_entry = [ is_numeric($id) ? $id : 0 ];
+	$sql_entry = 'SELECT *, SUBSTRING(date_occurred, 1, 10) AS date_occurred FROM development WHERE '.( is_numeric($id) ? 'id=?' : 'is_issue=? OR is_issue IS NULL' ).' ORDER BY development.date_occurred DESC LIMIT 1';
 	$stmt_entry = $pdo->prepare($sql_entry);
-	$stmt_entry->execute([ $id ]);
+	$stmt_entry->execute($values_entry);
 	$entry = $stmt_entry->fetch();
 	
 	// Get additional info
@@ -56,7 +76,12 @@ if( is_numeric($id) ) {
 		$entry['user'] = $access_user->access_user([ 'id' => $entry['user_id'], 'get' => 'name' ]);
 		
 		// If regular dev entry, do some additional formatting
-		if( !$entry['is_issue'] ) {
+		if( !$entry['is_issue'] && $template != 'update' ) {
+			
+			$content = $entry['content'];
+			
+			// Parse entry
+			$content = $markdown_parser->parse_markdown($content);
 			
 			// Format change type
 			$content = preg_replace_callback('/'.'\[(Addition|Bug fix|Change|Feature)\]'.'/', function($matches) {
@@ -64,7 +89,9 @@ if( is_numeric($id) ) {
 			}, $content);
 			
 			// Format affected folder
-			$content = preg_replace('/'.'\<code\>([A-z]+)\<\/code\>'.'/', '<span class="any--weaken">$1</span>', $content);
+			$content = preg_replace('/'.'\<code\>([A-z\.]+)\<\/code\>'.'/', '<span class="entry__affects any--weaken">$1</span>', $content);
+			
+			$entry['content'] = $content;
 			
 		}
 		
@@ -73,14 +100,51 @@ if( is_numeric($id) ) {
 	// If no entry, redirect to index
 	else {
 		$template = 'index';
+		$error = 'No entry was found.';
 	}
 	
 }
 
 //
+// Get past entries
+//
+if( $template === 'all' ) {
+	
+	$sql_entries = 'SELECT title, id, SUBSTRING(development.date_occurred, 1, 10) AS date_occurred FROM development WHERE is_issue=? OR is_issue IS NULL ORDER BY date_occurred DESC';
+	$stmt_entries = $pdo->prepare($sql_entries);
+	$stmt_entries->execute([ 0 ]);
+	$entries = $stmt_entries->fetchAll();
+	
+	// Reorder entries by date (year and month descending, day ascending)
+	foreach($entries as $entry) {
+		
+		list($year, $month, $day) = explode( '-', $entry['date_occurred'] );
+		$tmp_entries[$year][$month] = $tmp_entries[$year][$month] ?: [];
+		array_unshift($tmp_entries[$year][$month], $entry);
+		
+	}
+	
+	$entries = $tmp_entries;
+	unset($tmp_entries);
+	
+}
+
+// ==============================================
+
+//
 // Display issues
 //
 if( $template === 'issues' ) {
+	
+	// Format issues
+	foreach(['completed', 'incomplete'] as $key) {
+		foreach($issues[$key] as $issue_key => $issue) {
+			
+			// Get user
+			$issues[$key][$issue_key]['user'] = $access_user->access_user([ 'id' => $issue['user_id'], 'get' => 'name' ]);
+			
+		}
+	}
 	
 	include('../development/page-issues.php');
 	
@@ -93,9 +157,6 @@ elseif( $template === 'entry' ) {
 	
 	// Get comments
 	$entry['comments'] = $access_comment->access_comment([ 'id' => $entry['id'], 'type' => 'development', 'get' => 'all' ]);
-	
-	// Parse entry
-	$entry['content'] = $markdown_parser->parse_markdown($entry['content']);
 	
 	// Wrap side-by-side images into gallery
 	$gallery_pattern = '((?:<div class="module module--image.*?<\/div>\n?){2,})';
@@ -119,21 +180,18 @@ elseif( $template === 'update' ) {
 }
 
 //
+// Display past updates
+//
+elseif( $template === 'all' ) {
+	
+	include('../development/page-all.php');
+	
+}
+
+//
 // Display index
 //
 else {
-	
-	// Get latest entry
-	$sql_entry = 'SELECT id, title, friendly, content, SUBSTRING(date_occurred,1,10) AS date_occurred FROM development WHERE is_issue=? ORDER BY date_occurred DESC LIMIT 1';
-	$stmt_entry = $pdo->prepare($sql_entry);
-	$stmt_entry->execute([ 0 ]);
-	$entry = $stmt_entry->fetch();
-	
-	// Get user
-	$entry['user'] = $access_user->access_user([ 'id' => $entry['user_id'], 'get' => 'name' ]);
-	
-	// Parse entry
-	$entry['content'] = $markdown_parser->parse_markdown($entry['content']);
 	
 	// Wrap side-by-side images into gallery
 	$gallery_pattern = '((?:<div class="module module--image.*?<\/div>\n?){2,})';
@@ -142,6 +200,9 @@ else {
 		"<div class=\"module--gallery-wrapper\"><div class=\"module--gallery any--scrollbar\">\n$1</div></div>\n",
 		$entry['content']
 	);
+	
+	// Limit number of issues
+	$issues = $issues['incomplete'] ? array_slice($issues['incomplete'], 0, 5) : null;
 	
 	include('../development/page-index.php');
 
