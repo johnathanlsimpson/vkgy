4e335cd16aa53d5cb5c947612622e1018a4bc7db
John Simpson <johnathan.l.simpson@gmail.com>
2021-02-01 21:20:17 -0500

    [Change] Cleaned up logic of image template.

--- images/template-image.php
+++ images/template-image.php
@@ -8,9 +8,7 @@
 				ob_start();
 				
 				?>
-					<li class="image__template any--flex" data-get="image_status" data-get-into="data-image-status" x-data="{ description: '{description}', addFace: false, showDescription: false, showArtists: false, showMusicians: false, showReleases: false }" x-init="$watch('description', () => { triggerChange($refs.description); })">
-						
-						
+					<li class="image__template any--flex" data-get="image_status" data-get-into="data-image-status" x-data="{ description: '{description}', imageContent: '{checked_image_type}', addFace: false, showDescription: false, showArtists: false, showMusicians: false, showReleases: false, tagStyle: '', extension: '{extension}', imageContent: '{checked_image_type}' }" x-init="$watch('description', () => { triggerChange($refs.description); }); $watch('showMusicians', value => { if(value) { $dispatch('show-faces'); } })">
 						
 						<div class="input__row" style="display: flex; flex-direction: column; margin-right: 1rem; align-items:flex-start;">
 							
@@ -25,12 +23,12 @@
 							<div class="input__group">
 								<label class="input__radio">
 									<input class="input__choice" name="image_is_default" type="radio" value="1" {is_default} />
-									<span class="symbol__unchecked">main image</span>
+									<span class="symbol__unchecked">default</span>
 								</label>
 							</div>
 							
-							<div class="input__group" style="margin-bottom:auto;">
-								<button class="input__button symbol__copy" x-on:click.prevent="copyMarkdown($refs.markdown)">copy code</button>
+							<div class="input__group" style="margin-top:auto;">
+								<button class="input__button symbol__copy" x-on:click.prevent="copyMarkdown($refs.markdown)">copy</button>
 							</div>
 							
 							<!-- Delete -->
@@ -50,7 +48,7 @@
 							<input                                                                        name="image_is_queued"       value="{is_queued}" hidden />
 							<input class="any--hidden"      data-get="image_is_new" data-get-into="value" name="image_is_new"          value="0" disabled hidden />
 							<input                                                                        name="image_face_boundaries" value='{face_boundaries}' hidden />
-							<input class="image__extension" data-get="image_extension"                                                 value="{extension}" hidden />
+							<input class="image__extension" data-get="image_extension"                                                 value="{extension}" x-model="extension" hidden />
 							
 							<!-- Description, default, delete -->
 							<div class="input__row image__message">
@@ -77,13 +75,12 @@
 										foreach(access_image::$allowed_image_contents as $value => $key) {
 											?>
 												<label class="input__radio">
-													<input class="input__choice" name="image_type[{id}]" type="radio" value="<?= $value; ?>" {checked_image_type:<?= $value; ?>} x-on:change="description = getDescription($el)" />
+													<input class="input__choice" name="image_type[{id}]" type="radio" value="<?= $value; ?>" {checked_image_type:<?= $value; ?>} x-on:change="description = getDescription($el); imageContent = $el.querySelector('[name^=image_type]:checked').value; if(imageContent == 1) { showMusicians = true; }" />
 													<span class="symbol__unchecked"><?= $key; ?></span>
 												</label>
 											<?php
 										}
 									?>
-
 								</div>
 							</div>
 
@@ -123,22 +120,21 @@
 									
 									<label class="input__label">Tag photo</label>
 									
-									<a class="image__tag symbol__plus" data-tag-type="musicians" x-on:click.prevent="showMusicians=true; $dispatch('show-faces');" x-show="!showMusicians">musicians</a>
-									<a class="image__tag symbol__plus" data-tag-type="releases" x-on:click.prevent="showReleases=true" x-show="!showReleases">releases</a>
-									<a class="image__tag symbol__plus" data-tag-type="artists" x-on:click.prevent="showArtists=true" x-show="!showArtists">artists</a>
+									<a class="image__tag symbol__plus" data-tag-type="musicians" x-on:click.prevent="showMusicians=true;" x-show="!showMusicians">musicians</a>
+									<a class="image__tag symbol__plus" data-tag-type="releases" x-on:click.prevent="showReleases=true; tagStyle='order:-1;'" x-show="!showReleases">releases</a>
+									<a class="image__tag symbol__plus" data-tag-type="artists" x-on:click.prevent="showArtists=true; tagStyle=''" x-show="!showArtists">artists</a>
 									
 								</div>
 							</div>
 							
 							<!-- Tag artists -->
-							<div class="input__row image__selects" x-show="showArtists">
-								<div class="input__group any--flex-grow image__artists">
+							<div class="input__row image__selects" x-show="showArtists || showReleases">
+								<div class="input__group any--flex-grow image__artists" x-show="showArtists">
 									
 									<label class="input__label">Tag artists</label>
 									<select class="input" data-populate-on-click="true" data-multiple="true" data-source="artists" name="image_artist_id[]" placeholder="artists" x-on:change="description = getDescription($el)" multiple>{artist_ids}</select>
 									
 								</div>
-							</div>
 							
 							<!--<div class="input__group any--flex-grow image__labels">
 								<label class="input__label">Labels</label>
@@ -146,8 +142,7 @@
 							</div>-->
 							
 							<!-- Tag releases -->
-							<div class="input__row image__selects" x-show="showReleases">
-								<div class="input__group any--flex-grow image__releases">
+								<div class="input__group any--flex-grow image__releases" x-show="showReleases" x-bind:style="tagStyle">
 									
 									<label class="input__label">Tag releases</label>
 									<select class="input" data-populate-on-click="true" data-multiple="true" data-source="releases" name="image_release_id[]" placeholder="releases" multiple>{release_ids}</select>
@@ -156,16 +151,18 @@
 							</div>
 							
 							<!-- Tag musicians by face -->
-							<div class="input__row" x-show="showMusicians">
+							<div class="input__row" x-show="showMusicians && (imageContent == 1 || imageContent == 3)" >
 								<div class="input__group">
 									
 									<label class="input__label">Tag musicians by face</label>
 									
 									<div class="image__faces any--flex">
 										
-										<span class="face__loading loading">Detecting faces...</span>
+										<span class="face__loading loading any--hidden">Detecting faces...</span>
 										
-										<a class="face__add a--outlined a--padded" href="" x-on:click.prevent="addFace=true"><span class="symbol__plus symbol--standalone"></span></a>
+										{tagged_faces}
+										
+										<a class="face__add a--outlined a--padded any--hidden" x-on:click.prevent="addFace=true"><span class="symbol__plus symbol--standalone"></span></a>
 										
 									</div>
 									
@@ -173,7 +170,7 @@
 							</div>
 							
 							<!-- Mark additional faces -->
-							<div class="input__row" x-show="addFace">
+							<div class="input__row" x-show="addFace && (imageContent == 1 || imageContent == 3)">
 								<div class="input__group">
 									
 									<label class="input__label">Mark faces</label>
@@ -190,11 +187,15 @@
 							</div>
 							
 							<!-- Tag musicians w/out face -->
-							<div class="input__row" x-show="showMusicians">
+							<div class="input__row" x-show="(showMusicians && imageContent != 4) || imageContent == 2">
 								<div class="input__group any--flex-grow image__musicians">
 									
-									<label class="input__label">Musicians</label>
+									<label class="input__label">Tag <span x-show="imageContent == 1 || imageContent == 3">other</span> musician<span x-show="imageContent != 2">s</span></label>
 									<select class="input" data-populate-on-click="true" data-multiple="true" data-source="musicians" name="image_musician_id[]" placeholder="musicians" multiple>{musician_ids}</select>
+									
+									<div class="input__note symbol__help any--weaken-color" x-show="imageContent != 1 && imageContent != 3">
+										Musicians can be tagged by face in group photos or flyers.
+									</div>
 									
 								</div>
 							</div>
