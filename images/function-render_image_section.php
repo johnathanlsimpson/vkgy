f0e46ae54d9b9a6c2a500415f54ade6b6dba6595
John Simpson <johnathan.l.simpson@gmail.com>
2021-02-19 00:25:07 -0500

    [Feature] Updated image section to get specific musician/release lists for reach artist, and to set additional variables.

--- images/function-render_image_section.php
+++ images/function-render_image_section.php
@@ -62,13 +62,40 @@ function render_image_section($images, $args = []) {
 	
 	$default[$args['item_type']] = $args['item_id'];
 	
+	// Save a list of all artists tagged in this batch of images so that we can get those artists' releases and musicians
+	$all_artist_ids = [];
+	
+	// Save default artist to list of artist ids
+	if( strlen($default['artist']) ) {
+		$all_artist_ids[ $default['artist'] ] = $default['artist'];
+	}
+	
 	if(is_array($images) && !empty($images)) {
 		foreach($images as $image) {
 			
+			// Save artist ids to list so we can get release/musician json lists for them later
+			if( strlen($image['artist_ids']) ) {
+				foreach( explode(',', $image['artist_ids']) as $temp_artist_id ) {
+					if( is_numeric($temp_artist_id) ) {
+						
+						$all_artist_ids[ $temp_artist_id ] = $temp_artist_id;
+						
+					}
+				}
+			}
+			
 			$image['url'] = '/images/'.$image['id'].'.thumbnail.'.$image['extension'];
-			$tagged_faces = '';
+			
+			// Attempt to set save a 'main artist' for this image so the musician and release selects can set e.g. data-source="musicians_1"
+			if( strlen($image['artist_ids']) ) {
+				$artist_id = explode(',', $image['artist_ids'])[0];
+			}
+			else {
+				$artist_id = $default['artist'];
+			}
 			
 			// For musicians tagged to image by face, get the html box for each
+			$tagged_faces = '';
 			if( is_array($image['musicians']) && !empty($image['musicians']) ) {
 				foreach( $image['musicians'] as $musician ) {
 					
@@ -77,6 +104,7 @@ function render_image_section($images, $args = []) {
 						'face'          => json_decode( $musician['face_boundaries'], true ),
 						'musician_id'   => $musician['musician_id'],
 						'musician_name' => is_numeric($musician['musician_id']) ? '(tagged)' : null,
+						'artist_id'     => $artist_id,
 					]);
 					
 				}
@@ -90,6 +118,7 @@ function render_image_section($images, $args = []) {
 				'credit'             => $image['credit'],
 				'is_exclusive'       => $image['is_exclusive'] ? 'checked' : null,
 				'is_default'         => $image['id'] == $args['default_id'] ? 'checked' : null,
+				'artist_id'          => $artist_id,
 				'artist_ids'         => render_options(($image['artist_ids'] ?: $default['artist']), $artist_list),
 				'blog_id'            => render_options(($image['blog_id'] ?: $default['blog']), $blog_list),
 				'musician_ids'       => render_options(($image['label_ids'] ?: $default['label']), $label_list),
@@ -104,12 +133,25 @@ function render_image_section($images, $args = []) {
 				'checked_image_type' => $image['image_content'],
 				'extension'          => $image['extension'],
 				'tagged_faces'       => $tagged_faces,
+				'is_previous_upload' => true,
+				'is_facsimile'       => $args['item_type'] != $image['item_type'] ? '1' : null, // If we eventually want to make facsimiles different in image list somehow; currently this is only set to 1 (= can't edit) if auto-populating image in blog post
+				'artist_is_set'      => $image['artist_ids'] || $default['artist']
 			]);
 			
 		}
 	}
 	else {
 		$rendered_images = [];
+	}
+	
+	// Loop through all tagged artists and get release/musician json lists
+	if( is_array($all_artist_ids) && !empty($all_artist_ids) ) {
+		foreach($all_artist_ids as $artist_id) {
+			
+			render_json_list( 'musician', $artist_id, 'artist_id', null, null, [ 'append_id' => true ] );
+			render_json_list( 'release', $artist_id, 'artist_id', null, null, [ 'append_id' => true ] );
+			
+		}
 	}
 	
 	$args['no_default'] = is_numeric($args['default_id']) ? null : 'checked';
