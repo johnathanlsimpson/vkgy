aa6999ab42a0b2b23fff686e9f76ea9cce8e7aa8
John Simpson <johnathan.l.simpson@gmail.com>
2021-03-21 18:38:54 -0400

    [Feature] Rewrote artist history section and filtering for it. Also placed it in same column as lineup.

--- artists/page-artist.php
+++ artists/page-artist.php
@@ -1,5 +1,7 @@
 <?php
 
+$large_header = true;
+
 // Page setup
 include_once('../artists/head.php');
 
@@ -8,12 +10,10 @@ style([
 ]);
 
 script([
-	'/artists/script-page-artist.js',
-]);
-
-script([
+	'/scripts/external/script-list.js',
 	'/scripts/script-pronounce.js',
 	'/artists/script-partial-sidebar.js',
+	'/artists/script-page-artist.js',
 ]);
 
 // Show removed page with message
@@ -45,13 +45,9 @@ if($artist_is_removed) {
 			</h2>
 
 			<?php
-				// If artist viewable, and requested videos, show videos
-				if($artist_is_viewable && $_GET['section'] === 'videos') {
-					//include('partial-videos.php');
-				}
 
 				// If artist viewable, but default view requested, show everything else
-				elseif($artist_is_viewable) {
+				if($artist_is_viewable) {
 
 					// Exclusive banner
 					if($artist_is_exclusive) {
@@ -81,7 +77,205 @@ if($artist_is_removed) {
 					if($artist['musicians']) {
 						include('partial-lineup.php');
 					}
+					
+					// History
+					if( $artist['history'] ) {
+						
+						// Count history items to see if we need to collapse area
+						foreach( $artist['history'] as $year ) {
+
+							foreach( $year as $month ) {
+
+								foreach( $month as $day ) {
+
+									foreach( $day as $line ) {
+
+										$num_history_lines++;
+
+										if( $num_history_lines === 50 ) {
+
+											break;
+
+										}
+
+									}
+
+								}
+
+							}
+
+						}
+						
+						?>
+							
+							<h2 class="h1" id="history">
+								<?= lang('History', '活動', 'div'); ?>
+							</h2>
+							
+							<input class="obscure__input" id="show-history" type="checkbox" <?= $num_history_lines >= 2 ? 'checked' : null; ?> />
+							<div class="history__container text obscure__container obscure--height obscure--faint" <?= $num_history_lines >= 2 ? 'style="min-height:10vh;"' : null; ?> >
+								
+								<details class="history__filters">
+
+									<summary class="filters__control">
+										
+										<span class="filters__open input__button symbol__filter">filter</span>
+										<span class="filters__close input__button close">close</span>
+										
+									</summary>
+
+									<div class="input__row">
+
+										<div class="input__group">
+											<label class="input__label">sort</label>
+											<button class="filters__sort sort" data-sort="sort-date" data-order="desc">Date</button>
+										</div>
+										
+										<div class="input__group">
+											
+											<label class="input__label">show</label>
+											<label class="input__radio">
+												<input class="input__choice filter" type="radio" value="all" checked />
+												<span class="symbol__unchecked">all</span>
+											</label>
+
+											<?php foreach( [ 'activity', 'release', 'member', 'live', 'other' ] as $filter_name ): ?>
+												<label class="input__checkbox">
+													<input class="input__choice filter" type="checkbox" value="<?= $filter_name; ?>" />
+													<span class="symbol__unchecked"><?= $filter_name; ?></span>
+												</label>
+											<?php endforeach; ?>
+
+										</div>
+										
+									</div>
+
+								</details>
+								
+								<ul class="history__list list">
+									<?php
+										foreach( $artist['history'] as $year => $months ) {
+											foreach( $months as $month => $days ) {
+												foreach( $days as $day => $items ) {
+													foreach( $items as $item_key => $item ) {
+
+														// Modify omnibus links
+														if( in_array( 'is_uneditable', $item['type'] ) && in_array( 'release', $item['type'] ) && strpos( $item['content'], '/releases/omnibus/' ) !== false ) {
+															$item['content'] = preg_replace('/'.'(releases\/omnibus\/\d+\/[A-z0-9-]*\/)'.'/', '$1&prev_next_artist='.$artist['id'], $item['content']);
+														}
+
+														// Classes for li
+														$li_classes = [];
+
+														if( $item_key ) {
+															$li_classes[] = 'dupe-date';
+														}
+
+														if( $year != $previous_year ) {
+															$li_classes[] = 'new-year';
+														}
+
+														$li_classes = implode(' ', $li_classes);
+
+														// Classes for content
+														$span_classes = [];
+
+														foreach( [ 'is_uneditable', 'lineup', 'setlist' ] as $weakened_type ) {
+															if( in_array( $weakened_type, $item['type'] ) ) {
+																$span_classes[] = 'any--weaken';
+															}
+														}
+
+														if( count($item['type']) === 1 && $item['type'][0] === 'note' ) {
+															$span_classes[] = 'any--weaken';
+														}
+
+														$span_classes = array_merge( $span_classes, $item['type'] );
+
+														$span_classes = implode(' ', array_unique($span_classes));
+														
+														// Add count to date to preserve order for same-day items when sorting
+														$sort_date = $item['date_occurred'].'-'.$item_key;
+
+														?>
+															<li class="history__item any--flex <?= $li_classes; ?>" data-year="<?= $year; ?>" data-sort-date="<?= $sort_date; ?>">
+																<h4 class="history__date date">
+																	<?= $item['date_occurred']; ?>
+																</h4>
+																<div class="history__content <?= $span_classes; ?>">
+																	<?= $item['content']; ?>
+																</div>
+															</li>
+														<?php
+
+														$previous_year = $year;
+
+													}
+												}
+											}
+										}
+									?>
+								</ul>
+								
+								<label class="input__button obscure__button" for="show-history">show full history</label>
+								
+							</div>
+							
+						<?php
+					}
+			
+					// Schedule
+					if($artist['lives']) {
+						?>
+							<span id="schedule"></span>
+							<h2><?= lang('Live history', 'ライブ一覧', 'div'); ?></h2>
+
+							<div class="text text--outlined">
+								<?php
+									foreach($artist['lives'] as $year => $schedule_year) {
+										?>
+											<details>
+												<summary class="h2" style="padding-bottom:0;">
+													<?= $year; ?>
+												</summary>
+												<ul class="obscure__item ul--compact" style="margin:1rem 0;">
+													<?php
+														foreach($schedule_year as $month => $schedule_month) {
+															foreach($schedule_month as $day => $schedule_day) {
+																?>
+																	<li class="any--weaken">
+																		<span class="h4"><?php
+																			echo '<a class="a--inherit" href="/lives/&date_occurred='.$year.'">'.$year.'</a>';
+																			echo '-';
+																			echo '<a class="a--inherit" href="/lives/&date_occurred='.$year.'-'.$month.'">'.$month.'</a>';
+																			echo '-';
+																			echo '<a class="a--inherit" href="/lives/&date_occurred='.$year.'-'.$month.'-'.$day.'">'.$day.'</a>';
+																		?></span>
+																		&nbsp;
+																		<?php
+																			foreach($schedule_day as $live_key => $live) {
+																				echo $live_key ? ', ' : null;
+																				echo '<a class="a--inherit" href="/lives/&id='.$live['id'].'">'.lang(($live['area_romaji'] ?: $live['area_name']), $live['area_name'], 'hidden').' '.lang(($live['livehouse_romaji'] ?: $live['livehouse_name']), $live['livehouse_name'], 'hidden').'</a>';
+																				echo $_SESSION['can_add_data'] ? '<a class="symbol__edit a--inherit" href="/lives/'.$live['id'].'/edit/" style="margin-left:1ch;">Edit</a>' : null;
+																			}
+																		?>
+																	</li>
+																<?php
+															}
+														}
+													?>
+												</ul>
+											</details>
+										<?php
+									}
+								?>
+							</div>
+							</details>
+						<?php
+					}
+					
 				}
+				
 			?>
 		</div>
 
@@ -94,179 +288,146 @@ if($artist_is_removed) {
 			<div class="text text--outlined">
 				<?php include('partial-stats.php'); ?>
 			</div>
-
-		</div>
-		
-		<!-- history -->
-		
-			<h2 class="h1">
-				<?= lang('History', '活動', 'div'); ?>
-			</h2>
-		<div>
 			
-		</div>
-		<div>
 			<?php
+				// Similarly named artists
+				// Get artists with same name or same pronunciation, then combine results (to eliminate duplicates) and remove current artist from result
+				$possible_dupes_by_name = $access_artist->access_artist([ 'name' => $artist['name'], 'get' => 'name', 'associative' => true ]);
+				$possible_dupes_by_pronunciation = strlen($artist['pronunciation']) ? $access_artist->access_artist([ 'name' => $artist['pronunciation'], 'get' => 'name', 'associative' => true ]) : [];
+				$possible_dupes = (is_array($possible_dupes_by_name) ? $possible_dupes_by_name : []) + (is_array($possible_dupes_by_pronunciation) ? $possible_dupes_by_pronunciation : []);
+				unset($possible_dupes[$artist['id']]);
 
-				// History
-				if($artist['history']) {
+				if(is_array($possible_dupes) && !empty($possible_dupes)) {
 					?>
-						<span id="history"></span>
-						<input id="history__all" name="history__filter" type="radio" checked hidden />
-						<input id="history__activity" name="history__filter" type="radio" hidden />
-						<input id="history__release" name="history__filter" type="radio" hidden />
-						<input id="history__member" name="history__filter" type="radio" hidden />
-						<input id="history__live" name="history__filter" type="radio" hidden />
-						<input id="history__other" name="history__filter" type="radio" hidden />
-
-						<?php
-							// Count history items to see if we need to collapse area
-							if( $artist['history'] ) {
-
-								foreach( $artist['history'] as $year ) {
-
-									foreach( $year as $month ) {
-
-										foreach( $month as $day ) {
-
-											foreach( $day as $line ) {
-
-												$num_history_lines++;
-
-												if( $num_history_lines === 50 ) {
-
-													break;
-
-												}
-
-											}
-
-										}
-
+						<h3>
+							<?= lang('Similarly named', '同じ名前', 'div'); ?>
+						</h3>
+						<div class="text text--outlined any--weaken-color">
+							<?php
+								foreach($possible_dupes as $dupe_key => $dupe) {
+									if(strlen($dupe['romaji'])) {
+										echo lang(
+											'<a class="artist a--inherit" href="/artists/'.$dupe['friendly'].'/">'.$dupe['romaji'].'</a> ('.$dupe['name'].')',
+											'<a class="artist a--inherit" href="/artists/'.$dupe['friendly'].'/">'.$dupe['name'].'</a>',
+											'hidden'
+										);
+									}
+									else {
+										echo '<a class="artist a--inherit" href="/artists/'.$dupe['friendly'].'/">'.$dupe['name'].'</a>';
 									}
 
+									echo '&nbsp;<span class="any--weaken">'.$dupe['friendly'].'</span><br />';
 								}
+							?>
+						</div>
+					<?php
+				}
 
-							}
-						?>
-			
-						
-						<input class="obscure__input" id="show-history" type="checkbox" <?= $num_history_lines >= 50 ? 'checked' : null; ?> />
-			
-						<div class="text bio__container obscure__container obscure--height obscure--faint" style="min-height:60vh;">
+				// Links
+				if($artist["urls"]) {
+					?>
+						<h3><?= lang('Links', 'リンク', 'div'); ?></h3>
+						<input class="obscure__input" id="obscure-links" type="checkbox" <?= count($artist['urls']) > 4 ? 'checked' : null; ?> >
+						<div class="any--weaken text text--outlined obscure__container obscure--faint">
+							<ul>
+								<?php
+									foreach($artist["urls"] as $url) {
+										?>
+											<li class="obscure__item">
+												<a class="<?= $url['platform'] ? 'symbol__'.$url['platform'] : null; ?>" href="<?= $url['content']; ?>" target="_blank"><?= $url['display_name']; ?></a>
+												<a class="a--inherit" href="http://web.archive.org/web/1/<?= $url['content']; ?>" target="_blank">(saved)</a>
+												<?php
+													if(is_array($url['musician']) && !empty($url['musician'])) {
+														echo '<div>';
+														echo ['?', 'V', 'G', 'B', 'D', 'K', '?', 'S'][$url['musician']['position']].'. ';
+														if($url['musician']['as_name']) {
+															echo $url['musician']['as_romaji'] ? lang($url['musician']['as_romaji'], $url['musician']['as_name'], 'hidden') : $url['musician']['as_name'];
+														}
+														else {
+															echo $url['musician']['romaji'] ? lang($url['musician']['romaji'], $url['musician']['name'], 'hidden') : $url['musician']['name'];
+														}
+														echo '</div>';
+													}
+												?>
+											</li>
+										<?php
+									}
+								?>
+							</ul>
+							<label class="input__button obscure__button" for="obscure-links">Show all</label>
+						</div>
+					<?php
+				}
+
+				// Label history
+				if($artist["labels"]) {
+					?>
+						<h3><?= lang('Label history', '所属レーベル', 'div'); ?></h3>
+						<div class="label__container any--margin">
 							<?php
-								foreach($artist['history'] as $y => $history_year) {
+								foreach($artist["labels"] as $period_key => $period) {
 									?>
-										<ul>
-											<h2><?= $y; ?></h2>
+										<div class="label__period">
 											<?php
-												foreach($history_year as $history_month) {
-													foreach($history_month as $history_day) {
-														foreach($history_day as $history_event_num => $history_event) {
-															if(strlen($history_event['content'])) {
-
-																// For certain types, set font to weaken
-																$event_class = null;
-																foreach(['is_uneditable', 'lineup', 'setlist'] as $weaken_type) {
-																	if(in_array($weaken_type, $history_event['type'])) {
-																		$event_class = 'any--weaken';
-
-																		if(in_array('is_uneditable', $history_event['type']) && in_array('release', $history_event['type'])) {
-																			if(strpos($history_event['content'], '/releases/omnibus/') !== false) {
-																				$history_event['content'] = preg_replace('/'.'(releases\/omnibus\/\d+\/[A-z0-9-]*\/)'.'/', '$1&prev_next_artist='.$artist['id'], $history_event['content']);
-																			}
-																		}
+												foreach($period as $organization_key => $organization) {
+													?>
+														<div class="label__organization">
+															<?php
+																foreach($organization as $company_key => $company) {
+																	?>
+																		<div class="label__company any--weaken-size">
+																			<?php
+																				if(!empty($company["friendly"])) {
+																					?>
+																						<a class="symbol__company" href="/labels/<?php echo $company["friendly"]; ?>/">
+																							<?php echo $company["quick_name"]; ?>
+																						</a>
+																					<?php
+																					echo $company["romaji"] ? " (".$company["name"].")" : null;
+																				}
+																				else {
+																					echo $company["quick_name"].($company["romaji"] ? " (".$company["name"].")" : null);
+																				}
+																				if(is_array($company["notes"])) {
+																					foreach($company["notes"] as $note) {
+																						?>
+																							<span class="any__note"><?php echo $note; ?></span>
+																						<?php
+																					}
+																				}
+																			?>
+																		</div>
+																	<?php
+																	if($company_key + 1 < count($organization)) {
+																		?>
+																			<div class="label__company label__line"><span class="symbol__line"></span></div>
+																		<?php
 																	}
 																}
-																if(count($history_event['type']) === 1 && $history_event['type'][0] === 'note') {
-																	$event_class = 'any--weaken';
-																}
-
-																?>
-																	<li class="bio__item" data-item-num="<?= $history_event_num; ?>" data-item-type="<?= implode(' ', $history_event['type']); ?>">
-																		<h4 class="bio__date <?= $history_event_num ? 'bio__date--multiple' : null; ?>"><?= $history_event['date_occurred']; ?></h4>
-																		<div class="bio__content <?= $event_class; ?>">
-																			<h5 class="bio__title"></h5>
-																			<?= $history_event['content']; ?>
-																		</div>
-																	</li>
-																<?php
-															}
-														}
+															?>
+														</div>
+													<?php
+													if($organization_key + 1 < count($period)) {
+														?>
+															<span class="label__comma any--weaken-color"> &amp; </span>
+														<?php
 													}
 												}
 											?>
-										</ul>
+										</div>
 									<?php
-								}
-							?>
-							
-						<label class="input__button obscure__button" for="show-history">show full history</label>
-						</div>
-					<?php
-				}
-			
-				// Schedule
-				if($artist['lives']) {
-					?>
-						<span id="schedule"></span>
-						<h2><?= lang('Live history', 'ライブ一覧', 'div'); ?></h2>
-						
-						<div class="text text--outlined">
-							<?php
-								foreach($artist['lives'] as $year => $schedule_year) {
-									?>
-										<details>
-											<summary class="h2" style="padding-bottom:0;">
-												<?= $year; ?>
-											</summary>
-											<ul class="obscure__item ul--compact" style="margin:1rem 0;">
-												<?php
-													foreach($schedule_year as $month => $schedule_month) {
-														foreach($schedule_month as $day => $schedule_day) {
-															?>
-																<li class="any--weaken">
-																	<span class="h4"><?php
-																		echo '<a class="a--inherit" href="/lives/&date_occurred='.$year.'">'.$year.'</a>';
-																		echo '-';
-																		echo '<a class="a--inherit" href="/lives/&date_occurred='.$year.'-'.$month.'">'.$month.'</a>';
-																		echo '-';
-																		echo '<a class="a--inherit" href="/lives/&date_occurred='.$year.'-'.$month.'-'.$day.'">'.$day.'</a>';
-																	?></span>
-																	&nbsp;
-																	<?php
-																		foreach($schedule_day as $live_key => $live) {
-																			echo $live_key ? ', ' : null;
-																			echo '<a class="a--inherit" href="/lives/&id='.$live['id'].'">'.lang(($live['area_romaji'] ?: $live['area_name']), $live['area_name'], 'hidden').' '.lang(($live['livehouse_romaji'] ?: $live['livehouse_name']), $live['livehouse_name'], 'hidden').'</a>';
-																			echo $_SESSION['can_add_data'] ? '<a class="symbol__edit a--inherit" href="/lives/'.$live['id'].'/edit/" style="margin-left:1ch;">Edit</a>' : null;
-																		}
-																	?>
-																</li>
-															<?php
-														}
-													}
-												?>
-											</ul>
-										</details>
-									<?php
+									if($period_key + 1 < count($artist["labels"])) {
+										?>
+											<span class="symbol__down-caret label__next"></span>
+										<?php
+									}
 								}
 							?>
 						</div>
-						</details>
 					<?php
 				}
-			
-			
 			?>
-		</div>
-		
-		<div>
-			<label class="input__radio symbol__unchecked" for="history__all"><?= lang('all', '全て', 'hidden'); ?></label>
-			<label class="input__radio symbol__unchecked" for="history__activity"><?= lang('activity', '活動', 'hidden'); ?></label>
-			<label class="input__radio symbol__unchecked" for="history__release"><?= lang('release', 'リリース', 'hidden'); ?></label>
-			<label class="input__radio symbol__unchecked" for="history__member"><?= lang('member change', 'メンバーチェンジ', 'hidden'); ?></label>
-			<label class="input__radio symbol__unchecked" for="history__live"><?= lang('live', 'イベント', 'hidden'); ?></label>
-			<label class="input__radio symbol__unchecked" for="history__other"><?= lang('other', 'その他', 'hidden'); ?></label>
+
 		</div>
 		
 	</div>
--- artists/script-page-artist.js
+++ artists/script-page-artist.js
@@ -29,3 +29,169 @@ if(sessionElems.length) {
 		});
 	});
 }
+
+
+	// Set up filtering/ordering options
+ let filterOptions = {
+  valueNames: [
+			'activity',
+			'date',
+			'end',
+			'lineup',
+			'live',
+			'member',
+			'note',
+			'release',
+			'schedule',
+			'start',
+			'trouble',
+			{ 'data' : [ 'year', 'sort-date' ] }
+		]
+ };
+	
+	// Set up filterable list
+	let historyContainer = document.querySelector('.history__container');
+
+ let historyItems = new List(historyContainer, filterOptions);
+	
+	// Set up dynamic filters
+	let filtersContainer = document.querySelector('.history__filters');
+	
+	filtersContainer.addEventListener('change', function(event) {
+		
+		if( event.target.classList.contains('filter') ) {
+			
+			let allElem = filtersContainer.querySelector('[value="all"]');
+			let allIsChecked = allElem.checked;
+			let otherElems = filtersContainer.querySelectorAll('[value]:not([value="all"])');
+			let otherCheckedElems = filtersContainer.querySelectorAll('[value]:not([value="all"]):checked');
+			
+			if(
+				( event.target.value === 'all' && allIsChecked ) // if person just checked 'all' (can't uncheck 'cause it's radio)
+				||
+				( !otherCheckedElems || !otherCheckedElems.length ) // if all other filters unchecked, default back to all being checked
+			) {
+				
+				// Make sure 'all' is checked
+				allElem.checked = true;
+				
+				// Make sure other filters aren't checked as the same time as 'all'
+				otherElems.forEach(function(otherElem) {
+					otherElem.checked = false;
+				});
+				
+				// Reset filter
+				historyItems.filter();
+				
+			}
+			
+			// If 'all' isn't checked and some other filter is checked, filter appropriately
+			else {
+				
+				// Make sure 'all' is unchecked
+				allElem.checked = false;
+				
+				// Loop through list elems
+				historyItems.filter(function(item) {
+					
+					// By default, assume item will be hidden
+					let itemSatisfiesFilter = false;
+					
+					// We'll save the active filters here and use them in a bit
+					let activeFilters = [];
+					
+					// Loop through filter buttons and see which filters we need to push to list
+					otherCheckedElems.forEach(function(checkedElem) {
+						
+						let checkedFilter = checkedElem.value;
+						
+						// Push the filter button's value as the filter
+						activeFilters.push( checkedFilter );
+						
+						// Certain filter buttons may activate further hidden filters
+						if( checkedFilter === 'activity' ) {
+							activeFilters.push( 'start', 'end' );
+						}
+						
+						else if( checkedFilter === 'other' ) {
+							activeFilters.push( 'label', 'name', 'trouble', 'note', 'setlist' );
+						}
+						
+						else if( checkedFilter === 'member' ) {
+							activeFilters.push( 'lineup' );
+						}
+						
+					});
+					
+					// Now for the active filters, loop through and actually filter them
+					activeFilters.forEach(function(activeFilter) {
+						
+						if( item.values()[activeFilter] ) {
+							
+							itemSatisfiesFilter = true;
+							
+						}
+						
+					});
+					
+					return itemSatisfiesFilter;
+					
+				});
+				
+			}
+			
+		}
+		
+	});
+	
+	// When filters are changed, make sure year separators are correct
+	historyItems.on('updated', function() {
+		
+		let currentYear, previousYear, currentDay, previousDay = '';
+		
+		historyItems.visibleItems.forEach(function(item) {
+			
+			currentYear = item.values().year;
+			currentDay = item.values().date;
+			
+			if( currentYear != previousYear ) {
+				item.elm.classList.add('new-year');
+			}
+			else {
+				item.elm.classList.remove('new-year');
+			}
+			
+			if( currentDay === previousDay ) {
+				item.elm.classList.add('same-day');
+			}
+			else {
+				item.elm.classList.remove('same-day');
+			}
+			
+			previousYear = currentYear;
+			previousDay = currentDay;
+			
+		});
+		
+		// Scroll back to top
+		location.hash = null;
+		location.hash = 'history';
+		
+	});
+	
+	// Account List.js issue where initial sort appears to do nothing since
+	// it's sorted in the php; so we'll make the button initially sort the
+	// opposite direction then remove that attribute so it toggles directions
+	let sortElem = filtersContainer.querySelector('.sort');
+	
+	sortElem.addEventListener('click', function() {
+		
+		// Let List.js do its thing, then change the button
+		setTimeout(function() {
+			sortElem.removeAttribute('data-order');
+		}, 300);
+		
+		// Also blur the button
+		sortElem.blur();
+		
+	});
