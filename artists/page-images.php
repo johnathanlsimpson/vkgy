dd25b6c88849089957131f4b02f1c270ea374bd7
John Simpson <johnathan.l.simpson@gmail.com>
2021-03-23 18:27:10 -0400

    [Change] Made quick grid layout for images.

--- artists/page-images.php
+++ artists/page-images.php
@@ -18,15 +18,39 @@ subnav([
 ?>
 
 <div class="col c1">
-	
-	<div style="display: grid; grid-template-columns: repeat( auto-fit, minmax(200px, 1fr) );grid-gap: 1rem;grid-template-rows: masonry;">
 			
+			
+			<div style="  display: grid;
+    grid-template-columns: repeat(auto-fill, minmax(240px, 1fr));
+    grid-gap: 1rem;
+    grid-auto-rows: minmax(200px, auto);
+    grid-auto-flow: dense;
+    padding: 10px;">
+				
+	
 			<?php foreach($images as $image): ?>
 				
-				<a style="" href="<?= $image['url']; ?>"><img class="lazy" data-src="<?= $image['thumbnail_url']; ?>" height="<?= $image['height'].'px'; ?>" width="<?= $image['width'].'px'; ?>" style="width:100%;height:auto;" /></a>
+				<?php
+				$ratio = $image['height'] && $image['width'] ? $image['height'] / $image['width'] : 1;
+				
+				if( $ratio > 1) {
+					$x = 'grid-row: span 2;';
+				}
+				if( $ratio > 2) {
+					$x = 'grid-row: span 3;';
+				}
+				if( $ratio < 1 && $ratio > 0.6) {
+					$x = 'grid-row: span 2; grid-column: span 2;';
+				}
+				if( $ratio < 0.6 ) {
+					$x = 'grid-column: span 3;';
+				}
+				
+				?>
+				
+				<a class="lazy" style="<?= $x; ?>background-size:cover;background-color:hsl(var(--background));background-position:center;" data-src="<?= $image['small_url']; ?>" href="<?= $image['url']; ?>" height="<?= $image['height'].'px'; ?>" width="<?= $image['width'].'px'; ?>"></a>
 				
 			<?php endforeach; ?>
-		
 	</div>
 	
 </div>
