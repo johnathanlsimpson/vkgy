bfbbcdab6e53ac0ed3e35e65b2e6b3785ef4c476
John Simpson <johnathan.l.simpson@gmail.com>
2021-04-03 16:48:03 -0400

    [Change] Completely rewrote musician controller to clean it up.

--- musicians/index.php
+++ musicians/index.php
@@ -1,33 +1,38 @@
 <?php
-	breadcrumbs([
-		"Musicians" => "/musicians/"
-	]);
+
+// ============================================
+// Setup
+// ============================================
+
+include_once('../php/class-tag.php');
+
+$access_musician = new access_musician($pdo);
+$access_image = new access_image($pdo);
+$access_tag = new tag($pdo);
+
+$musician_id = is_numeric($_GET['id']) ? $_GET['id'] : null;
+$musician_is_removed = false;
+
+$action = sanitize($_GET['action']);
+$action = in_array( $action, [ 'add', 'edit' ] ) ? $action : null;
+
+include_once('../musicians/head.php');
+
+// ============================================
+// Get data
+// ============================================
+
+// Get musician data
+if( is_numeric($musician_id) ) {
 	
-	subnav([
-		lang('Musician list', 'ミュージシャン一覧', ['secondary_class' => 'any--hidden']) => '/musicians/'
-	]);
+	// Main data
+	$musician = $access_musician->access_musician([ 'id' => $musician_id, 'get' => 'all' ]);
 	
-	subnav([
-		lang('Add musician', 'ミュージシャン追加', ['secondary_class' => 'any--hidden']) => '/musicians/add/',
-	], 'interact', true);
-	
-	$page_header = lang('Musicians', 'ミュージシャン', ['container' => 'div']);
-	
-	$access_musician = new access_musician($pdo);
-	
-	if(is_numeric($_GET["musician"])) {
-		$musician_id = sanitize($_GET["musician"]);
-		$musician = $access_musician->access_musician(["id" => $musician_id, "get" => "all"]);
+	// Additional data
+	if( is_array($musician) && !empty($musician) ) {
 		
 		// Get tags
-		// ============================================
-		include_once('../php/class-tag.php');
-		
-		$item_type = 'musician';
-		$item_id = $musician['id'];
-		
-		$access_tag = new tag($pdo);
-		$tags = $access_tag->access_tag([ 'item_type' => $item_type, 'item_id' => $item_id, 'get' => 'all', 'separate' => true ]);
+		$tags = $access_tag->access_tag([ 'item_type' => 'musician', 'item_id' => $musician_id, 'get' => 'all', 'separate' => true ]);
 		
 		// Loop through tags and set some flags
 		if( is_array($tags) && !empty($tags) && is_array($tags['tagged']) ) {
@@ -46,45 +51,145 @@
 			}
 		}
 		
-		// Navigation
-		// ============================================
-		if(is_array($musician) && !empty($musician)) {
-			breadcrumbs([$musician["quick_name"] => "/musicians/".$musician["friendly"]."/"]);
-			
-			for($i = 0; $i < count($musician["history"]); $i++) {
-				if(!empty($musician["history"][$i]["friendly"])) {
-					subnav([
-						"Edit musician" => "/artists/".$musician["history"][$i]["friendly"]."/edit/"
-					]);
-				}
-			}
-			
-			// Get musician's images
-			$sql_images = 'SELECT * FROM images_musicians LEFT JOIN images ON images.id=images_musicians.image_id WHERE images_musicians.musician_id=?';
-			$stmt_images = $pdo->prepare($sql_images);
-			$stmt_images->execute([ $musician['id'] ]);
-			$images = $stmt_images->fetchAll();
-			
-			$pageTitle = $musician["quick_name"]." musician profile | ".$musician["name"]."&#12503;&#12525;&#12501;&#12451;&#12540;&#12523;";
-			
-			include("../musicians/page-musician.php");
-		}
-		else {
-			$error = "Sorry, that musician doesn't exist. Showing musicians list instead.";
-		}
+		// Get musician's images
+		$images = $access_image->access_image([ 'musician_id' => $musician_id, 'get' => 'all' ]);
+		
 	}
 	
-	if(!is_array($musician) || empty($musician)) {
-		if($_GET["action"] === "add") {
-			breadcrumbs(["Add musician" => "/musicians/add/"]);
-			
-			$pageTitle = "Add musician";
-			
-			include("../musicians/page-add.php");
+}
+
+// ============================================
+// Decide which template to display and set errors
+// ============================================
+
+// Add musician
+if( $action === 'add' ) {
+	
+	if( $_SESSION['can_add_data'] ) {
+		$template = 'add';
+	}
+	else {
+		$error = 'Sorry, you don\'t have permission to add musicians.';
+	}
+	
+}
+
+// Edit musician
+if( $action === 'edit' ) {
+	
+	if( $_SESSION['can_add_data'] ) {
+		
+		if( is_array($musician) && !empty($musician) ) {
+			$template = 'edit';
 		}
 		else {
-			$pageTitle = "Musician list | &#20491;&#20154;&#32034;&#24341;";
-			include("../musicians/page-index.php");
+			$error = 'Sorry, that musician doesn\'t exist. Showing musician search instead.';
 		}
+		
 	}
-?>
+	else {
+		$error = 'Sorry, you don\'t have permission to edit musicians.';
+	}
+	
+}
+
+// View musician
+if( !$action && is_numeric($musician_id) ) {
+	
+	if( is_array($musician) && !empty($musician) ) {
+		
+		if( !$musician_is_removed || ( $musician_is_removed && $_SESSION['can_approve_data'] ) ) {
+			
+			$template = 'musician';
+			
+			if( $musician_is_removed ) {
+				$error = 'This musician has been removed from the public. Please don\'t share any information on this page.';
+			}
+			
+		}
+		else {
+			$error = 'Sorry, that musician has been removed.';
+		}
+		
+	}
+	else {
+		$error = 'Sorry, that musician doesn\'t exist. Showing musician search instead.';
+	}
+	
+}
+
+// Index (default template)
+if( !$template ) {
+	$template = 'index';
+}
+
+// ============================================
+// Show template
+// ============================================
+
+// Add musician
+if( $template === 'add' ) {
+	
+	$page_title = 'Add musician | ミュージシャン追加';
+	
+	breadcrumbs([
+		lang('Add musician', 'ミュージシャン追加', 'hidden') => '/musicians/add/'
+	]);
+	
+	include('../musicians/page-add.php');
+	
+}
+
+// Edit musician
+if( $template === 'edit' ) {
+	
+	$page_title = 'Edit '.($musician['romaji'] ?: $musician['name']).' | '.$musician['name'].'を編集する';
+	
+	$page_header = lang('Edit musician', 'メンバーを編集する', 'div');
+	
+	breadcrumbs([
+		lang('Edit musician', 'ミュージシャンを編集する', 'hidden') => '/musicians/'.$musician['id'].'/edit/'
+	]);
+	
+	subnav([
+		lang('Edit musician', 'メンバーを編集する', 'hidden') => '/musicians/'.$musician['id'].'/edit/'
+	]);
+	
+	include('../musicians/page-edit.php');
+	
+}
+
+// Show musician
+if( $template === 'musician' ) {
+	
+	subnav([
+		lang('Profile', 'プロフィール', 'hidden') => '/musicians/'.$musician['id'].'/'.$musician['friendly'].'/'
+	]);
+	
+	if( $_SESSION['can_add_data'] ) {
+		subnav([
+			lang('Edit musician', 'ミュージシャンを編集する', 'hidden') => '/musicians/'.$musician['id'].'/edit/'
+		], 'interact', true);
+	}
+	
+	// Page image
+	if(is_array($images) && !empty($images)) {
+		$page_image = 'https://vk.gy/images/'.$images[0]['id'].($images[0]['friendly'] ? '-'.$images[0]['friendly'] : null).'.'.$images[0]['extension'];
+	}
+	
+	$page_header = lang( ($musician['romaji'] ?: $musician['name']).' profile', $musician['name'].'のプロフィール', 'div' );
+	
+	$page_title = ($musician['romaji'] ?: $musician['name']).' profile | '.$musician['name'].'のプロフィール';
+	
+	include('../musicians/page-musician.php');
+	
+}
+
+// Index (default view)
+if( $template === 'index' ) {
+	
+	$active_page = '/musicians/';
+	
+	include('../musicians/page-index.php');
+	
+}
