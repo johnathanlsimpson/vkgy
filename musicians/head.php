9f26796134477fe27a8f91b10d81757b4c9f7c59
John Simpson <johnathan.l.simpson@gmail.com>
2021-04-03 16:46:29 -0400

    [Change] Standardized head and added Japanese test.

--- musicians/head.php
+++ musicians/head.php
@@ -1,13 +1,18 @@
 <?php
-	$pageTitle = "Musicians";
-	
-	breadcrumbs([
-		"Musicians" => "/musicians/"
-	]);
-	
-	if($_SESSION["can_add_data"]) {
-		subnav([
-			"Add" => "/musicians/add/"
-		]);
-	}
-?>
+
+// Fallback title
+$page_title = 'Musicians | メンバー';
+
+breadcrumbs([
+	lang('Musicians', 'メンバー', 'hidden') => '/musicians/'
+]);
+
+subnav([
+	lang('Find a musician', 'ミュージシャンの検索', 'hidden') => '/musicians/'
+]);
+
+if( $_SESSION['can_add_data'] ) {
+	subnav([
+		lang('Add musicians', 'ミュージシャン追加', 'hidden') => '/musicians/add/',
+	], 'interact', true);
+}
