07b587b0dc10496772b986f221ecdda9f4c67513
John Simpson <johnathan.l.simpson@gmail.com>
2021-05-11 22:30:03 -0400

    [Change] Changed quick search behavior: searches for single characters will now only return exact matches (kana characters are exempt); searches greater than 3 characters will return fuzzier results (i.e. mize will return La'veil MizeriA); and kana searches will no longer return nonsensical results (due to the friendly version being - which matches almost anything).

--- php/function-quick_search.php
+++ php/function-quick_search.php
@@ -1,4 +1,4 @@
-<?php
+​<?php
 
 include_once('../php/include.php');
 
@@ -6,9 +6,12 @@ function quick_search( $search_term ) {
 	
 	global $pdo;
 	
-	if( strlen($search_term) ) {
+	$search_term_length = strlen($search_term);
+	
+	if( $search_term_length ) {
 		
 		$friendly_search_term = friendly( $search_term );
+		$friendly_search_term = $friendly_search_term != '-' ? $friendly_search_term : null;
 		
 		// Values are same for both queries
 		$values = [
@@ -46,36 +49,81 @@ function quick_search( $search_term ) {
 		$stmt_exact->execute( $values );
 		$exact_artists = $stmt_exact->fetchAll(PDO::FETCH_UNIQUE|PDO::FETCH_ASSOC);
 		
-		// Fuzzy search
-		$sql_fuzzy = '
-			SELECT
-				artists.id,
-				artists.name,
-				artists.romaji,
-				artists.friendly,
-				IF( images.id IS NOT NULL, CONCAT("/images/", images.id, ".thumbnail.", images.extension), "" ) AS thumbnail_url
-			FROM
-				artists
-			LEFT JOIN images ON images.id=artists.image_id
-			WHERE
-				artists.is_vkei>?
-				AND (
-					artists.name LIKE CONCAT(?, "%")
-					OR
-					artists.romaji LIKE CONCAT(?, "%")
-					OR
-					artists.pronunciation LIKE CONCAT(?, "%")
-					OR
-					artists.friendly LIKE CONCAT(?, "%")
-				)
-			ORDER BY artists.friendly ASC
-			LIMIT 10';
-		$stmt_fuzzy = $pdo->prepare($sql_fuzzy);
-		$stmt_fuzzy->execute( $values );
-		$fuzzy_artists = $stmt_fuzzy->fetchAll(PDO::FETCH_UNIQUE|PDO::FETCH_ASSOC);
+		// Combine results
+		$artists = $exact_artists;
+		
+		// Fuzzy search--only if more than one letter is provided
+		if( $search_term_length > 1 ) {
+			
+			$sql_fuzzy = '
+				SELECT
+					artists.id,
+					artists.name,
+					artists.romaji,
+					artists.friendly,
+					IF( images.id IS NOT NULL, CONCAT("/images/", images.id, ".thumbnail.", images.extension), "" ) AS thumbnail_url
+				FROM
+					artists
+				LEFT JOIN images ON images.id=artists.image_id
+				WHERE
+					artists.is_vkei>?
+					AND (
+						artists.name LIKE CONCAT(?, "%")
+						OR
+						artists.romaji LIKE CONCAT(?, "%")
+						OR
+						artists.pronunciation LIKE CONCAT(?, "%")
+						OR
+						artists.friendly LIKE CONCAT(?, "%")
+					)
+				ORDER BY artists.friendly ASC
+				LIMIT 10';
+			$stmt_fuzzy = $pdo->prepare($sql_fuzzy);
+			$stmt_fuzzy->execute( $values );
+			$fuzzy_artists = $stmt_fuzzy->fetchAll(PDO::FETCH_UNIQUE|PDO::FETCH_ASSOC);
+			
+			// Combine results
+			$artists = $artists + $fuzzy_artists;
+			
+		}
+		
+		// Super fuzzy search--only if string is long enough and we don't have 10 results yet
+		if( count($artists) < 10 && $search_term_length > 3 ) {
+			
+			$sql_super_fuzzy = '
+				SELECT
+					artists.id,
+					artists.name,
+					artists.romaji,
+					artists.friendly,
+					IF( images.id IS NOT NULL, CONCAT("/images/", images.id, ".thumbnail.", images.extension), "" ) AS thumbnail_url
+				FROM
+					artists
+				LEFT JOIN images ON images.id=artists.image_id
+				WHERE
+					artists.is_vkei>?
+					AND (
+						artists.name LIKE CONCAT("%", ?, "%")
+						OR
+						artists.romaji LIKE CONCAT("%", ?, "%")
+						OR
+						artists.pronunciation LIKE CONCAT("%", ?, "%")
+						OR
+						artists.friendly LIKE CONCAT("%", ?, "%")
+					)
+				ORDER BY artists.friendly ASC
+				LIMIT 10';
+			$stmt_super_fuzzy = $pdo->prepare($sql_super_fuzzy);
+			$stmt_super_fuzzy->execute( $values );
+			$super_fuzzy_artists = $stmt_super_fuzzy->fetchAll(PDO::FETCH_UNIQUE|PDO::FETCH_ASSOC);
+			
+			// Combine results
+			$artists = $artists + $super_fuzzy_artists;
+			
+		}
 		
 		// Combine results
-		$artists = array_slice( $exact_artists + $fuzzy_artists, 0, 10 );
+		$artists = array_slice( $artists, 0, 10 );
 		
 	}
 	
